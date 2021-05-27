ecd90ef8c4ff248ddbb30ea68b4ae1a7ec39f6cc
romandubovskyi <rdubowskij@gmail.com>
2021-05-27 13:04:31 +0300

    added current conditions

---
+++ src/main/java/mate/academy/dao/TicketDao.java
@@ -0,0 +1,8 @@
+package mate.academy.dao;
+
+
+import mate.academy.model.Ticket;
+
+public interface TicketDao {
+    Ticket add(Ticket ticket);
+}
---
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -0,0 +1,59 @@
+package mate.academy.dao.impl;
+
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class ShoppingCartDaoImpl implements ShoppingCartDao {
+
+    @Override
+    public ShoppingCart add(ShoppingCart shoppingCart) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            Long shoppingCartId = (Long) session.save(shoppingCart);
+            transaction.commit();
+            shoppingCart.setId(shoppingCartId);
+            return shoppingCart;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Cannot create Shopping cart ", e);
+        }
+    }
+
+    @Override
+    public ShoppingCart getByUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<ShoppingCart> query = session.createQuery("FROM ShoppingCart WHERE user = "
+                    + ":user", ShoppingCart.class);
+            query.setParameter("user", user);
+            return query.uniqueResult();
+        } catch (Exception e) {
+            throw new DataProcessingException("Cannot find shopping cart using user ", e);
+        }
+    }
+
+    @Override
+    public void update(ShoppingCart shoppingCart) {
+        Transaction transaction = null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            session.update(shoppingCart);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Cannot create shopping cart ", e);
+        }
+    }
+}
