60301e252effd64809f1777ba82c05b606ee4895
romandubovskyi <rdubowskij@gmail.com>
2021-05-27 13:08:16 +0300

    fixed table name

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -11,7 +11,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table (name = "shopping_cart")
+@Table (name = "shopping_carts")
 public class ShoppingCart {
     @Id
     private Long id;
