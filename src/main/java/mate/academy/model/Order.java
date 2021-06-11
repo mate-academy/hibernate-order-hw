45b0b5f666a337fa7505754d8cf594933983bc3f
roman <nashpatsa@gmai.com>
2021-06-11 12:07:24 +0300

    add solution

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -1,25 +1,31 @@
 package mate.academy.model;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tickets")
-public class Ticket {
+@Table(name = "orders")
+public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne(fetch = FetchType.LAZY)
-    private MovieSession movieSession;
-    @ManyToOne(fetch = FetchType.LAZY)
+    @OneToMany
+    @JoinColumn(name = "order_id")
+    private List<Ticket> tickets;
+    private LocalDateTime orderDate;
+    @ManyToOne
     private User user;
-    private LocalDateTime showTime;
+
+    public Order() {
+    }
 
     public Long getId() {
         return id;
@@ -29,20 +35,20 @@ public class Ticket {
         this.id = id;
     }
 
-    public MovieSession getMovieSession() {
-        return movieSession;
+    public List<Ticket> getTickets() {
+        return tickets;
     }
 
-    public void setMovieSession(MovieSession movieSession) {
-        this.movieSession = movieSession;
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
     }
 
-    public LocalDateTime getShowTime() {
-        return showTime;
+    public LocalDateTime getOrderDate() {
+        return orderDate;
     }
 
-    public void setShowTime(LocalDateTime showTime) {
-        this.showTime = showTime;
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
     }
 
     public User getUser() {
@@ -55,11 +61,11 @@ public class Ticket {
 
     @Override
     public String toString() {
-        return "Ticket{"
+        return "Order{"
                 + "id=" + id
-                + ", movieSession=" + movieSession
+                + ", tickets=" + tickets
+                + ", orderDate=" + orderDate
                 + ", user=" + user
-                + ", showTime=" + showTime
                 + '}';
     }
 }
