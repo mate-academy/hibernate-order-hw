c87c6ffae64a655055a3be8bf55a788380ea510f
Andrii Romaniuk <romaniukandrii7@gmail.com>
2021-06-11 17:39:00 +0300

    id generation fix

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
@@ -15,12 +17,12 @@ import javax.persistence.Table;
 @Table(name = "orders")
 public class Order {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToMany
     private List<Ticket> tickets;
     private LocalDateTime orderDate;
     @ManyToOne(fetch = FetchType.LAZY)
-    @MapsId
     private User user;
 
     public Long getId() {
