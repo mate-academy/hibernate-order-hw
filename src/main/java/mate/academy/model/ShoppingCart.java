5ea995bf0e5ae8186b76363806a2c7bf83abb976
PashaTerletskyi <p.terletsky@gmail.com>
2021-08-10 23:21:19 +0300

    Fixed checkstyle

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -11,7 +11,7 @@ import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table (name = "shopping_carts")
+@Table(name = "shopping_carts")
 public class ShoppingCart {
     @Id
     private Long id;
