9863f03df52ca0f7f7cf73a0f61ccb2635130eba
Yura Lug <LUV97@ukr.net>
2021-08-11 13:59:15 +0300

    Final reliz v. 1.0

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -1,28 +1,37 @@
 package mate.academy.model;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
-import javax.persistence.MapsId;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table (name = "shopping_carts")
-public class ShoppingCart {
+@Table(name = "orders")
+public class Order {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToMany
-    private List<Ticket> tickets;
-    @OneToOne
-    @MapsId
-    @JoinTable(name = "shopping_carts_tickets",
-            joinColumns = @JoinColumn(name = "shopping_cart_id"),
+    @JoinTable(name = "order_tickets",
+            joinColumns = @JoinColumn(name = "order_id"),
             inverseJoinColumns = @JoinColumn(name = "ticket_id"))
+    private List<Ticket> tickets;
+    @Column(name = "order_date")
+    private LocalDateTime orderDate;
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
+
+    public Order() {
+    }
 
     public Long getId() {
         return id;
@@ -40,6 +49,14 @@ public class ShoppingCart {
         this.tickets = tickets;
     }
 
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
     public User getUser() {
         return user;
     }
@@ -50,9 +67,10 @@ public class ShoppingCart {
 
     @Override
     public String toString() {
-        return "ShoppingCart{"
+        return "Order{"
                 + "id=" + id
                 + ", tickets=" + tickets
+                + ", orderDate=" + orderDate
                 + ", user=" + user
                 + '}';
     }
