c5ff1c99039bf41b53c366c7a424843699313b2b
Vitalii Martynov <VitaliiMvDeveloper@gmail.com>
2021-09-14 16:27:11 +0300

    Initial solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -2,16 +2,44 @@ package mate.academy;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
 import mate.academy.model.MovieSession;
+import mate.academy.model.Order;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import mate.academy.security.AuthenticationService;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
+import mate.academy.service.OrderService;
+import mate.academy.service.ShoppingCartService;
 
 public class Main {
-    public static void main(String[] args) {
-        MovieService movieService = null;
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
+    private static final MovieService movieService = (MovieService) injector
+            .getInstance(MovieService.class);
+
+    private static final CinemaHallService cinemaHallService = (CinemaHallService) injector
+            .getInstance(CinemaHallService.class);
+
+    private static final MovieSessionService movieSessionService = (MovieSessionService) injector
+            .getInstance(MovieSessionService.class);
+
+    private static final AuthenticationService authenticationService =
+            (AuthenticationService) injector.getInstance(AuthenticationService.class);
+
+    private static final ShoppingCartService shoppingCartService =
+            (ShoppingCartService) injector.getInstance(ShoppingCartService.class);
+
+    private static final OrderService orderService =
+            (OrderService) injector.getInstance(OrderService.class);
+
+    public static void main(String[] args) throws AuthenticationException {
+        injectUsers();
 
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
@@ -27,7 +55,6 @@ public class Main {
         secondCinemaHall.setCapacity(200);
         secondCinemaHall.setDescription("second hall with capacity 200");
 
-        CinemaHallService cinemaHallService = null;
         cinemaHallService.add(firstCinemaHall);
         cinemaHallService.add(secondCinemaHall);
 
@@ -44,12 +71,44 @@ public class Main {
         yesterdayMovieSession.setMovie(fastAndFurious);
         yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
 
-        MovieSessionService movieSessionService = null;
         movieSessionService.add(tomorrowMovieSession);
         movieSessionService.add(yesterdayMovieSession);
 
         System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
         System.out.println(movieSessionService.findAvailableSessions(
-                        fastAndFurious.getId(), LocalDate.now()));
+                fastAndFurious.getId(), LocalDate.now()));
+
+        injectShoppingCartsAndOrders();
+    }
+
+    private static void injectUsers() {
+        authenticationService.register("bob@gmail.com", "qwerty");
+        authenticationService.register("alice@gmail.com", "123456");
+        authenticationService.register("john@gmail.com", "johnny");
+    }
+
+    private static void injectShoppingCartsAndOrders() throws AuthenticationException {
+        User bob = authenticationService.login("bob@gmail.com", "qwerty");
+        MovieSession movieSession1 = movieSessionService.get(1L);
+        MovieSession movieSession2 = movieSessionService.get(2L);
+        shoppingCartService.addSession(movieSession1, bob);
+        shoppingCartService.addSession(movieSession1, bob);
+        shoppingCartService.addSession(movieSession2, bob);
+
+        User alice = authenticationService.login("alice@gmail.com", "123456");
+        shoppingCartService.addSession(movieSession2, alice);
+
+        ShoppingCart shoppingCartBob = shoppingCartService.getByUser(bob);
+        System.out.println("===================");
+        System.out.println(shoppingCartBob);
+
+        ShoppingCart shoppingCartAlice = shoppingCartService.getByUser(alice);
+        System.out.println("===================");
+        System.out.println(shoppingCartAlice);
+
+        Order bobOrder = orderService.completeOrder(shoppingCartBob);
+        System.out.println("===================");
+        System.out.println(bobOrder);
+        System.out.println(shoppingCartService.getByUser(bob));
     }
 }
