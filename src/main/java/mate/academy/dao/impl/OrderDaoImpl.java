73b718d51852bd56ab5d3623ae1791bdd5919d66
Kyrylo Dvornyk <kiril75341@gmail.com>
2021-10-28 15:11:09 +0300

    Implemented hibernate-order task.

---
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,58 @@
+package mate.academy.dao.impl;
+
+import java.util.List;
+import mate.academy.dao.OrderDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Order;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class OrderDaoImpl implements OrderDao {
+    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+    @Override
+    public Order add(Order order) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(order);
+            transaction.commit();
+            return order;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't create order " + order, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Order> getOrdersByUser(User user) {
+        String getOrdersByUserQuery = "SELECT o FROM Order o "
+                + "LEFT JOIN FETCH o.user "
+                + "LEFT JOIN FETCH o.tickets t "
+                + "LEFT JOIN FETCH t.movieSession ms "
+                + "LEFT JOIN FETCH ms.movie "
+                + "LEFT JOIN FETCH ms.cinemaHall "
+                + "WHERE o.user.id = :userId";
+        try (Session session = sessionFactory.openSession()) {
+            Query<Order> orderQuery = session.createQuery(getOrdersByUserQuery, Order.class);
+            orderQuery.setParameter("userId", user.getId());
+            return orderQuery.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get orders by user " + user, e);
+        }
+    }
+}
