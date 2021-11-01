6bb0d9b1027b4585f08013cf98b26fa0cb8b2dd8
Vladyslav Gural <vladgural@gmail.com>
2021-11-01 18:20:47 +0200

    fixed deleted data from table shopping_carts_tickets

--- src/main/java/mate/academy/service/impl/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -1,12 +1,14 @@
 package mate.academy.service.impl;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import mate.academy.dao.OrderDao;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.Order;
 import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
 import mate.academy.model.User;
 import mate.academy.service.OrderService;
 
@@ -19,7 +21,8 @@ public class OrderServiceImpl implements OrderService {
     public Order completeOrder(ShoppingCart shoppingCart) {
         Order order = new Order();
         order.setOrderDate(LocalDateTime.now());
-        order.setTickets(shoppingCart.getTickets());
+        List<Ticket> tickets = new ArrayList<>(shoppingCart.getTickets());
+        order.setTickets(tickets);
         order.setUser(shoppingCart.getUser());
         return orderDao.add(order);
     }
