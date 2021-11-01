ca8a0c8c4ed1023988208712db921de9572af5ce
Vladyslav Gural <vladgural@gmail.com>
2021-11-01 18:11:32 +0200

    working commit

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,14 +1,19 @@
 package mate.academy;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import mate.academy.exception.RegistrationException;
 import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
 import mate.academy.model.MovieSession;
+import mate.academy.model.Order;
+import mate.academy.model.User;
 import mate.academy.security.AuthenticationService;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
+import mate.academy.service.OrderService;
 import mate.academy.service.ShoppingCartService;
 import mate.academy.service.UserService;
 
@@ -20,6 +25,8 @@ public class Main {
             (CinemaHallService) injector.getInstance(CinemaHallService.class);
     private static final MovieSessionService movieSessionService =
             (MovieSessionService) injector.getInstance(MovieSessionService.class);
+    private static final OrderService orderService =
+            (OrderService) injector.getInstance(OrderService.class);
     private static final AuthenticationService authenticationService =
             (AuthenticationService) injector.getInstance(AuthenticationService.class);
     private static final UserService userService =
@@ -51,5 +58,25 @@ public class Main {
         yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
         movieSessionService.add(tomorrowMovieSession);
         movieSessionService.add(yesterdayMovieSession);
+
+        User bob = null;
+        User alis = null;
+        try {
+            bob = authenticationService.register("bob@gmail.com", "bob1234");
+            alis = authenticationService.register("alis@gmail.com", "alis1234");
+        } catch (RegistrationException e) {
+            System.out.println(e.getMessage());
+        }
+
+        shoppingCartService.addSession(tomorrowMovieSession, bob);
+        shoppingCartService.addSession(tomorrowMovieSession, bob);
+        System.out.println(shoppingCartService.getByUser(bob));
+        orderService.completeOrder(shoppingCartService.getByUser(bob));
+        System.out.println(shoppingCartService.getByUser(bob));
+        shoppingCartService.clearShoppingCart(shoppingCartService.getByUser(bob));
+        shoppingCartService.addSession(yesterdayMovieSession, bob);
+        orderService.completeOrder(shoppingCartService.getByUser(bob));
+        List<Order> ordersHistory = orderService.getOrdersHistory(bob);
+        ordersHistory.forEach(System.out::println);
     }
 }
---
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,54 @@
+package mate.academy.dao.impl;
+
+import java.util.List;
+import mate.academy.dao.OrderDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Order;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class OrderDaoImpl implements OrderDao {
+    @Override
+    public Order add(Order order) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(order);
+            transaction.commit();
+            return order;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert Order: " + order, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Order> findByUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Order> query = session.createQuery("SELECT DISTINCT o "
+                    + "FROM Order o "
+                    + "LEFT JOIN FETCH o.tickets t "
+                    + "LEFT JOIN FETCH t.movieSession ms "
+                    + "LEFT JOIN FETCH ms.movie "
+                    + "LEFT JOIN FETCH ms.cinemaHall "
+                    + "WHERE o.user.id = :userId", Order.class);
+            query.setParameter("userId", user.getId());
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find Orders by user: " + user, e);
+        }
+    }
+}
