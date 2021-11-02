34484eea2e4ccdc27d196046560741bfc1a98d32
Andrii Demchenko <avd998ne@gmail.com>
2021-11-02 16:14:04 +0200

    filled Main, fixed checkstyle

--- src/main/java/mate/academy/dao/OrderDao.java
+++ src/main/java/mate/academy/dao/OrderDao.java
@@ -2,7 +2,6 @@ package mate.academy.dao;
 
 import java.util.List;
 import mate.academy.model.Order;
-import mate.academy.model.ShoppingCart;
 import mate.academy.model.User;
 
 public interface OrderDao {
--- src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -42,13 +42,13 @@ public class OrderDaoImpl implements OrderDao {
     @Override
     public List<Order> getByUser(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Query<Order> query = session.createQuery("FROM Order o "
+            Query<Order> query = session.createQuery("SELECT DISTINCT o FROM Order o "
                     + "LEFT JOIN FETCH o.user "
                     + "LEFT JOIN FETCH o.tickets t "
                     + "LEFT JOIN FETCH t.movieSession ms "
                     + "LEFT JOIN FETCH ms.movie "
                     + "LEFT JOIN FETCH ms.cinemaHall "
-                    + "WHERE t.user =:user", Order.class);
+                    + "WHERE o.user =:user", Order.class);
             query.setParameter("user", user);
             return query.list();
         } catch (Exception e) {
