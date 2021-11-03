09b8aad7a3dd52b3bd5ab4ae2ed5c951a9c963ed
Vitalii Mykh <vitaliymykh@gmail.com>
2021-11-03 13:28:47 +0200

    all tasks are done

---
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -0,0 +1,43 @@
+package mate.academy.service.impl;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import mate.academy.dao.OrderDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.Order;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
+import mate.academy.model.User;
+import mate.academy.service.OrderService;
+import mate.academy.service.ShoppingCartService;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Inject
+    private OrderDao orderDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
+
+    @Override
+    public Order completeOrder(ShoppingCart shoppingCart) {
+        List<Ticket> tickets = shoppingCart.getTickets();
+        User user = shoppingCart.getUser();
+        Order order = new Order();
+        order.setTickets(new ArrayList<>(tickets));
+        order.setUser(user);
+        order.setOrderDate(LocalDateTime.now());
+        shoppingCartService.clearShoppingCart(shoppingCart);
+        return orderDao.save(order);
+    }
+
+    @Override
+    public List<Order> getOrdersHistory(User user) {
+        List<Order> usersOrders = orderDao.getByUser(user);
+        return usersOrders.stream()
+                .filter(order -> order.getOrderDate().isBefore(LocalDateTime.now()))
+                .collect(Collectors.toList());
+    }
+}
