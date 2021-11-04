96e941e72ebf1fa20aa7b55e721e7e27a38eb7a4
Nazar Hetun <nazarhetun@gmail.com>
2021-11-04 15:05:51 +0200

    implement order service

---
+++ src/main/java/mate/academy/dao/OrderDao.java
@@ -0,0 +1,11 @@
+package mate.academy.dao;
+
+import java.util.List;
+import mate.academy.model.Order;
+import mate.academy.model.User;
+
+public interface OrderDao {
+    Order save(Order order);
+
+    List<Order> getOrdersHistory(User user);
+}
