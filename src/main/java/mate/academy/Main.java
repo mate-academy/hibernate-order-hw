539f4ba84dc7c4c3c06d642261999776b79baaa8
Yaroslav Yarmoshyk <yarmoshyk96@gmail.com>
2021-11-19 08:24:23 +0200

    added solution

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,17 +1,28 @@
 package mate.academy;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import mate.academy.exception.AuthenticationException;
+import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
 import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import mate.academy.security.AuthenticationService;
+import mate.academy.security.AuthenticationServiceImpl;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
+import mate.academy.service.OrderService;
+import mate.academy.service.ShoppingCartService;
+import mate.academy.service.UserService;
 
 public class Main {
+    private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
-        MovieService movieService = null;
+        MovieService movieService = (MovieService) injector
+                .getInstance(MovieService.class);
 
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
@@ -27,12 +38,10 @@ public class Main {
         secondCinemaHall.setCapacity(200);
         secondCinemaHall.setDescription("second hall with capacity 200");
 
-        CinemaHallService cinemaHallService = null;
+        CinemaHallService cinemaHallService = (CinemaHallService) injector
+                .getInstance(CinemaHallService.class);
         cinemaHallService.add(firstCinemaHall);
         cinemaHallService.add(secondCinemaHall);
-
-        System.out.println(cinemaHallService.getAll());
-        System.out.println(cinemaHallService.get(firstCinemaHall.getId()));
 
         MovieSession tomorrowMovieSession = new MovieSession();
         tomorrowMovieSession.setCinemaHall(firstCinemaHall);
@@ -44,12 +53,39 @@ public class Main {
         yesterdayMovieSession.setMovie(fastAndFurious);
         yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
 
-        MovieSessionService movieSessionService = null;
+        MovieSessionService movieSessionService = (MovieSessionService) injector
+                .getInstance(MovieSessionService.class);
         movieSessionService.add(tomorrowMovieSession);
         movieSessionService.add(yesterdayMovieSession);
 
-        System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
-        System.out.println(movieSessionService.findAvailableSessions(
-                        fastAndFurious.getId(), LocalDate.now()));
+        User bob = new User();
+        bob.setEmail("email1@gmai.com");
+        bob.setPassword("34");
+
+        User alice = new User();
+        alice.setEmail("email2@gmai.com");
+        alice.setPassword("qqw");
+
+        AuthenticationService authenticationService = new AuthenticationServiceImpl();
+        authenticationService.register(bob.getEmail(),bob.getPassword());
+        authenticationService.register(alice.getEmail(),alice.getPassword());
+        try {
+            authenticationService.login("email2@gmai.com","qqw");
+        } catch (AuthenticationException e) {
+            throw new RuntimeException("Email or password is incorrect");
+        }
+        UserService userService = (UserService) injector
+                .getInstance(UserService.class);
+        bob.setId(userService.findByEmail(bob.getEmail()).get().getId());
+        ShoppingCartService shoppingCartService = (ShoppingCartService) injector
+                .getInstance(ShoppingCartService.class);
+
+        shoppingCartService.addSession(tomorrowMovieSession,bob);
+        ShoppingCart bobsCart = shoppingCartService.getByUser(bob);
+
+        OrderService orderService = (OrderService) injector
+                .getInstance(OrderService.class);
+        System.out.println(orderService.completeOrder(bobsCart));
+        System.out.println(bobsCart);
     }
 }
