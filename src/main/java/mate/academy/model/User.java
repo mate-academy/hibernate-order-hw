4575f1e89e27bb97fcdbf1be27ad1da56b8465e4
DmytroPetrushenko <dmytro.petrushenko@gmail.com>
2021-12-28 14:55:55 +0200

    task done. mvn Ok!

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -49,4 +49,12 @@ public class User {
     public void setSalt(byte[] salt) {
         this.salt = salt;
     }
+
+    @Override
+    public String toString() {
+        return "user{"
+                + "id: " + id
+                + "email: " + email
+                + "}";
+    }
 }
