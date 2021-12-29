4a3365dc7a3cbbc067753151ebed5be82288eec3
Olha Tankevych <freakystyley85@gmail.com>
2021-12-29 13:26:26 +0200

    added Order, OrderDao, OrderService

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -18,6 +18,14 @@ public class User {
     private String password;
     private byte[] salt;
 
+    @Override
+    public String toString() {
+        return "User{"
+                + "id=" + id
+                + ", email='" + email + '\''
+                + '}';
+    }
+
     public Long getId() {
         return id;
     }
