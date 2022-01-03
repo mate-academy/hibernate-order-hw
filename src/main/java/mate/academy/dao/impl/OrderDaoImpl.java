ffae31bf5763cacbf8b4330afdbfbe437eb552fd
Artik <Artik25942@gmail.com>
2022-01-03 21:55:40 +0200

    builSucces

--- src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -36,7 +36,7 @@ public class OrderDaoImpl implements OrderDao {
 
     @Override
     public List<Order> getByUser(User user) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()){
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query<Order> query = session.createQuery("FROM Order o "
                     + "LEFT JOIN FETCH o.user u "
                     + "LEFT JOIN FETCH o.tickets  t "
