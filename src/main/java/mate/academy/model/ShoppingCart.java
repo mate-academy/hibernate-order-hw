d326c39624961f787c80cd8ea95690985dec35d6
Vladyslav Chyver <lione2009lione@gmail.com>
2022-01-04 15:58:54 +0200

    mvn clean package

--- src/main/java/mate/academy/model/ShoppingCart.java
+++ src/main/java/mate/academy/model/ShoppingCart.java
@@ -7,7 +7,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
-import javax.persistence.ManyToOne;
 import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
