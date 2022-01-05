2ced194ce662399b83c2bc02475194f49baab724
Vladyslav Chyver <lione2009lione@gmail.com>
2022-01-05 08:59:31 +0200

    Anotation Service was moved to implementation

--- src/main/java/mate/academy/service/OrderService.java
+++ src/main/java/mate/academy/service/OrderService.java
@@ -1,12 +1,10 @@
 package mate.academy.service;
 
 import java.util.List;
-import mate.academy.lib.Service;
 import mate.academy.model.Order;
 import mate.academy.model.ShoppingCart;
 import mate.academy.model.User;
 
-@Service
 public interface OrderService {
     Order completeOrder(ShoppingCart shoppingCart);
 
