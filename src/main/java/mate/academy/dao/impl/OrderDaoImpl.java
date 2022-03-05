dcd3591491d1ffd28b82708f36e99bb9125095d3
Oleh Kvasha <oleg.kvasha.00@gmail.com>
2022-03-06 00:50:36 +0200

    Add OrderDaoImpl

---
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,76 @@
+package mate.academy.dao.impl;
+
+import mate.academy.dao.OrderDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Order;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+@Dao
+public class OrderDaoImpl implements OrderDao {
+    @Override
+    public Order add(Order order) {
+        Session session = null;
+        Transaction transaction = null;
+
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(order);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add order: " + order, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return order;
+    }
+
+    @Override
+    public List<Order> getByUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory()
+                .openSession()) {
+            Query<Order> query = session.createQuery("SELECT o "
+                    + "FROM Order o "
+                    + "WHERE o.user = :user", Order.class);
+            query.setParameter("user", user);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get orders by user: "
+                    + user, e);
+        }
+    }
+
+    @Override
+    public void update(Order order) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.update(order);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't update order: "
+                    + order, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
