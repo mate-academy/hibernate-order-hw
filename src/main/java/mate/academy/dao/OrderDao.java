ca84dd935a069f35ff91e8665144559153ea4a4f
Andriy_Poluhovic <chimbar@ukr.net>
2022-03-25 18:01:07 +0200

    Was created classes OrderService and OrderServiceImpl

--- src/main/java/mate/academy/dao/OrderDao.java
+++ src/main/java/mate/academy/dao/OrderDao.java
@@ -4,9 +4,8 @@ import java.util.List;
 import mate.academy.model.Order;
 import mate.academy.model.User;
 
-
 public interface OrderDao {
     Order add(Order order);
 
-    List<Order> getOrdersHistory(User user);
+    List<Order> getByUser(User user);
 }
--- src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -37,7 +37,7 @@ public class OrderDaoImpl implements OrderDao {
     }
 
     @Override
-    public List<Order> getOrdersHistory(User user) {
+    public List<Order> getByUser(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()){
             Query<Order> getListOrderByQuery = session.createQuery("from Order o "
                     + "left join o.user "
