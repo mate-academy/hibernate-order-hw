110b742c624d2c17e37e5757c254caf1f4e1d2ac
vladsysa <vladsysa@mail.ru>
2022-04-12 17:41:15 +0200

    fixed commit 2

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -2,7 +2,16 @@ package mate.academy.model;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "orders")
@@ -17,6 +26,9 @@ public class Order {
     private List<Ticket> tickets;
     private LocalDateTime orderDate;
     @ManyToOne
+    @JoinTable(name = "users_orders",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
     @MapsId
     @JoinColumn(name = "id")
     private User user;
