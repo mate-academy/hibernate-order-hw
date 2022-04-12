5bc98ed17c48501311e37cbe153aacc8eaf0ed00
AprioriKS <sounddummies@gmail.com>
2022-04-12 15:39:01 +0300

    fix cs

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -39,7 +39,7 @@ public class Main {
         secondCinemaHall.setDescription("second hall with capacity 200");
 
         CinemaHallService cinemaHallService = (CinemaHallService) injector
-            .getInstance(CinemaHallService.class);
+                .getInstance(CinemaHallService.class);
         cinemaHallService.add(firstCinemaHall);
         cinemaHallService.add(secondCinemaHall);
 
@@ -57,22 +57,22 @@ public class Main {
         yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
 
         MovieSessionService movieSessionService = (MovieSessionService) injector
-            .getInstance(MovieSessionService.class);
+                .getInstance(MovieSessionService.class);
         movieSessionService.add(tomorrowMovieSession);
         movieSessionService.add(yesterdayMovieSession);
 
         System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
         System.out.println(movieSessionService.findAvailableSessions(
-            fastAndFurious.getId(), LocalDate.now()));
+                fastAndFurious.getId(), LocalDate.now()));
 
         AuthenticationService authenticationService = (AuthenticationService) injector
-            .getInstance(AuthenticationService.class);
+                .getInstance(AuthenticationService.class);
         UserService userService = (UserService) injector.getInstance(UserService.class);
         User user = authenticationService.register("sounddummies@gmail.com", "1234");
         System.out.println(userService.findByEmail("sounddummies@gmail.com").get());
 
         ShoppingCartService shoppingCartService = (ShoppingCartService) injector
-            .getInstance(ShoppingCartService.class);
+                .getInstance(ShoppingCartService.class);
         shoppingCartService.addSession(tomorrowMovieSession, user);
         ShoppingCart shoppingCart = shoppingCartService.getByUser(user);
         System.out.println(shoppingCart);
