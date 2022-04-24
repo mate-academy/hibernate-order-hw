7e52f2d75e80bd6d1f6b31a8ad8b5321f3c261e8
Valera Lemberov <valera.lemberov1993@gmail.com>
2022-04-24 21:11:01 +0300

    Found the missing comment from a previous review.

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -9,7 +9,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
-import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
