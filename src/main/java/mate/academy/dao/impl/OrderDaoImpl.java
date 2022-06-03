266e2387b4ac5fcf2a2838595905a7198184aa4f
Dmytro Skrypnyk <skripnik139@gmail.com>
2022-06-03 15:01:39 +0300

    fix mistake

--- src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -40,10 +40,10 @@ public class OrderDaoImpl implements OrderDao {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query<Order> query = session.createQuery("select distinct o "
                     + "from Order o "
-                    + "left join fetch o.tickets t "
-                    + "left join fetch t.movieSession m "
-                    + "left join fetch m.movie "
-                    + "left join fetch m.cinemaHall "
+                    + "join fetch o.tickets t "
+                    + "join fetch t.movieSession m "
+                    + "join fetch m.movie "
+                    + "join fetch m.cinemaHall "
                     + "where o.user = :user");
             query.setParameter("user", user);
             return query.getResultList();
