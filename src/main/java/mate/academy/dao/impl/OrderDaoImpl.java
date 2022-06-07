9efad84905d99e8635e14668e4d1709415372475
OLEH VIESHAIEV <sred_a@i.ua>
2022-06-07 14:03:52 +0300

    hibernate-order-hw solution was created

---
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,50 @@
+package mate.academy.dao.impl;
+
+import java.util.List;
+import mate.academy.dao.OrderDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Order;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class OrderDaoImpl implements OrderDao {
+    @Override
+    public Order add(Order order) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(order);
+            transaction.commit();
+            return order;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Couldn't add order " + order + " to Db", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Order> getByUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Order> orderQuery = session.createQuery("SELECT DISTINCT o "
+                    + "FROM Order o JOIN FETCH o.user JOIN FETCH o.tickets t "
+                    + "JOIN FETCH t.movieSession ms JOIN FETCH ms.movie "
+                    + "JOIN FETCH ms.cinemaHall WHERE o.user =: user", Order.class);
+            return orderQuery.setParameter("user", user).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Couldn't get order by user " + user, e);
+        }
+    }
+}
