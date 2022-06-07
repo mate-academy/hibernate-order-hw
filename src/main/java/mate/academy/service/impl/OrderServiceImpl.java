b4e897be1fe15c3dd2e85f15f6930a9ed45a3625
Nesstor Fedak <nesstortor@gmail.com>
2022-06-07 17:07:16 +0300

    mistakes was fixed

--- src/main/java/mate/academy/service/impl/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package mate.academy.service.impl;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import mate.academy.dao.OrderDao;
 import mate.academy.lib.Inject;
@@ -24,7 +25,7 @@ public class OrderServiceImpl implements OrderService {
         List<Ticket> tickets = shoppingCart.getTickets();
         User user = shoppingCart.getUser();
         Order order = new Order();
-        order.setTickets(tickets);
+        order.setTickets(new ArrayList<>(shoppingCart.getTickets()));
         order.setUser(user);
         order.setOrderDate(LocalDateTime.now());
         Order addedOrder = orderDao.add(order);
