b64bcfc91fee6fb5ed688d1b4641467f05e142ad
Vladyslav Fomenko <with.god.victorius@gmail.com>
2022-06-08 14:50:30 +0300

    removed rdundant annotation

--- src/main/java/mate/academy/dao/OrderDao.java
+++ src/main/java/mate/academy/dao/OrderDao.java
@@ -1,11 +1,9 @@
 package mate.academy.dao;
 
 import java.util.List;
-import mate.academy.lib.Dao;
 import mate.academy.model.Order;
 import mate.academy.model.User;
 
-@Dao
 public interface OrderDao {
     Order add(Order order);
 
