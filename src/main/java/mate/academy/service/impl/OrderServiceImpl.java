9174deb8e57460a581bf46f38dccb3c7e0efeb50
Konstantin Miroshnichenko <bigfishnotbad@gmail.com>
2022-07-26 18:04:24 +0300

    fixed according to CheckStyle file: invoked clearingShoppingCart method in cpompleteOrder method

--- src/main/java/mate/academy/service/impl/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package mate.academy.service.impl;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import mate.academy.dao.OrderDao;
@@ -9,8 +10,10 @@ import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.Order;
 import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
 import mate.academy.model.User;
 import mate.academy.service.OrderService;
+import mate.academy.service.ShoppingCartService;
 
 @Service
 public class OrderServiceImpl implements OrderService {
@@ -18,14 +21,18 @@ public class OrderServiceImpl implements OrderService {
     private OrderDao orderDao;
     @Inject
     private ShoppingCartDao shoppingCartDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
 
     @Override
     public Order completeOrder(ShoppingCart shoppingCart) {
         Order order = new Order();
         order.setOrderDate(LocalDateTime.now());
         order.setUser(shoppingCart.getUser());
-        order.setTickets(shoppingCart.getTickets());
+        List<Ticket> tickets = new ArrayList<>(shoppingCart.getTickets());
+        order.setTickets(tickets);
         orderDao.add(order);
+        shoppingCartService.clearShoppingCart(shoppingCart);
         return null;
     }
 
