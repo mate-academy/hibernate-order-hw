cfb0474241bbf69560e9e9db0f8f02c3fecae12b
Oleksandr Domnich <dmnichz@gmail.com>
2022-07-26 16:16:43 +0300

    added solution for hibernate-order-hw task

---
+++ src/main/java/mate/academy/dao/OrderDao.java
@@ -0,0 +1,13 @@
+package mate.academy.dao;
+
+import java.util.List;
+import mate.academy.model.Order;
+import mate.academy.model.User;
+
+public interface OrderDao {
+    Order add(Order order);
+
+    Order getByUser(User user);
+
+    List<Order> getOrdersHistory(User user);
+}
