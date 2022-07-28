0bc7cd645880b9dbaa791c094737ccff92d20005
Oleksii Kordysh <aleksey.kordysh@gmail.com>
2022-07-28 12:47:01 +0300

    checkstyle fixed

--- src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -9,7 +9,6 @@ import mate.academy.model.User;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.hibernate.query.Query;
 
 @Dao
 public class OrderDaoImpl implements OrderDao {
