b2c53e56c51a0522c6b94da361403503abfa3ef7
yaroslav <morozov.yrv@gmail.com>
2022-08-22 09:05:59 +0300

    order service

---
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -0,0 +1,20 @@
+package mate.academy.service.impl;
+
+import mate.academy.model.Order;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import mate.academy.service.OrderService;
+
+import java.util.List;
+
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public Order completeOrder(ShoppingCart shoppingCart) {
+        return null;
+    }
+
+    @Override
+    public List<Order> getOrdersHistory(User user) {
+        return null;
+    }
+}
