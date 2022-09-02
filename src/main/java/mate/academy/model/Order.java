47896c9face8720b3674e8457b01b57d884f1ee1
Oleksii Mospan <alexey.mospan@gmail.com>
2022-09-02 21:41:06 +0300

    Added import for annotation Column

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -1,5 +1,6 @@
 package mate.academy.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
