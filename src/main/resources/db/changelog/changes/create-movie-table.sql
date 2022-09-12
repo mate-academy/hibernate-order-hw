d7ebd098b6d85574419ba9d5f8a4b74e0caabfc2
Benatti <ssunica0@gmail.com>
2022-09-12 16:29:13 +0300

    removed liquibase

--- src/main/resources/db/changelog/changes/create-movie-table.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
---changeset benatti:create-movie-table splitStatements:true endDelimiter:;
-CREATE TABLE `movies` (
-                          `id` bigint NOT NULL AUTO_INCREMENT,
-                          `description` varchar(255) DEFAULT NULL,
-                          `title` varchar(255) DEFAULT NULL,
-                          PRIMARY KEY (`id`)
-);
---rollback DROP TABLE users
--- src/main/resources/db/changelog/changes/create-shopping-cart-table.sql
+++
@@ -1,8 +0,0 @@
---liquibase formatted sql
---changeset benatti:create-shopping-cart-table splitStatements:true endDelimiter:;
-CREATE TABLE `shopping_carts` (
-                                  `id` bigint NOT NULL,
-                                  PRIMARY KEY (`id`),
-                                  CONSTRAINT `FKc1fbrvff059ke4p8ce3hu38oa` FOREIGN KEY (`id`) REFERENCES `users` (`id`)
-);
---rollback DROP TABLE users
