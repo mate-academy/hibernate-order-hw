50deb76d65c467cf0335d55bdb01d8c9eb2d4fa7
Roman Pylypchuk <roman.pylypchuk@yahoo.com>
2022-09-16 17:29:16 +0300

    deleted unused import to fix project build failure

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -3,7 +3,6 @@ package mate.academy.model;
 import java.time.LocalDateTime;
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
