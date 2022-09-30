d1a464f8471b5b56390c782406912b24c82e02ce
amarkelovUA <java.developer.aleksandr@gmail.com>
2022-09-30 13:07:57 +0300

    Cleaning of the cart after compleiting the order is added

--- src/main/java/mate/academy/service/impl/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -9,17 +9,21 @@ import mate.academy.lib.Service;
 import mate.academy.model.Order;
 import mate.academy.model.ShoppingCart;
 import mate.academy.model.User;
+import mate.academy.service.ShoppingCartService;
 
 @Service
 public class OrderServiceImpl implements mate.academy.service.OrderService {
     @Inject
     private OrderDao orderDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
 
     @Override
     public Order completeOrder(ShoppingCart shoppingCart) {
         Order addedOrder = new Order();
         addedOrder.setUser(shoppingCart.getUser());
         addedOrder.setTickets(new ArrayList<>(shoppingCart.getTickets()));
+        shoppingCartService.clearShoppingCart(shoppingCart);
         addedOrder.setOrderDate(LocalDateTime.now());
         return orderDao.add(addedOrder);
     }
