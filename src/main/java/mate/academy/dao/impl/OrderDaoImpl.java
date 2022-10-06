292b37a6d6bfb044b7f5005e4a44ab5e07932d9d
Misha Brovkin <haasiakira@gmail.com>
2022-10-06 18:25:18 +0300

    order Dao implementation done

--- src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -2,17 +2,50 @@ package mate.academy.dao.impl;
 
 import java.util.List;
 import mate.academy.dao.OrderDao;
+import mate.academy.exception.DataProcessingException;
 import mate.academy.model.Order;
 import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class OrderDaoImpl implements OrderDao {
     @Override
     public Order add(Order order) {
-        return null;
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(order);
+            transaction.commit();
+            return order;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert a order: " + order, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     @Override
     public List<Order> getAllByUser(User user) {
-        return null;
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("select distinct o from Order o " +
+                            "inner join fetch o.tickets t " +
+                            "inner join fetch t.user u " +
+                            "inner join fetch t.movieSession ms " +
+                            "inner join fetch ms.cinemaHall c " +
+                            "inner join fetch ms.movie m " +
+                            "where o.user.id = :userId", Order.class)
+                    .setParameter("userId", user.getId())
+                    .getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Coldn't get orders by user: " + user, e);
+        }
     }
 }
