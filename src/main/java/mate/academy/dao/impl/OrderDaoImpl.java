104788d0a3e18ead29219c5e2e57fec75a7f632c
Misha Brovkin <haasiakira@gmail.com>
2022-10-06 18:14:07 +0300

    add order and orderDao classes

---
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,18 @@
+package mate.academy.dao.impl;
+
+import java.util.List;
+import mate.academy.dao.OrderDao;
+import mate.academy.model.Order;
+import mate.academy.model.User;
+
+public class OrderDaoImpl implements OrderDao {
+    @Override
+    public Order add(Order order) {
+        return null;
+    }
+
+    @Override
+    public List<Order> getAllByUser(User user) {
+        return null;
+    }
+}
