f9c68c666a65afd54c5fc9b118a769a80acc6fa2
Misha Brovkin <haasiakira@gmail.com>
2022-10-06 18:31:46 +0300

    order Service implementation done

--- src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -3,12 +3,14 @@ package mate.academy.dao.impl;
 import java.util.List;
 import mate.academy.dao.OrderDao;
 import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
 import mate.academy.model.Order;
 import mate.academy.model.User;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
+@Dao
 public class OrderDaoImpl implements OrderDao {
     @Override
     public Order add(Order order) {
