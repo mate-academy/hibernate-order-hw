128d6ed98948895c0b0dc355ed825d1ec76c7352
ArturZhadan <artur.zhadan.17@gmail.com>
2022-10-06 17:18:24 +0300

    changed left join to inner join

--- src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -39,11 +39,11 @@ public class OrderDaoImpl implements OrderDao {
     public List<Order> getOrdersHistory(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query<Order> query = session.createQuery("from Order o "
-                    + "left join fetch o.user "
-                    + "left join fetch o.tickets t "
-                    + "left join fetch t.movieSession ms "
-                    + "left join fetch ms.movie "
-                    + "left join fetch ms.cinemaHall "
+                    + "join fetch o.user "
+                    + "join fetch o.tickets t "
+                    + "join fetch t.movieSession ms "
+                    + "join fetch ms.movie "
+                    + "join fetch ms.cinemaHall "
                     + "where o.user = :user", Order.class);
             query.setParameter("user", user);
             return query.getResultList();
