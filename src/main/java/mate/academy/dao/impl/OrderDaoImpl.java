380a551c87097cdf4a8863b7cdd966173736603a
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-10-07 13:50:12 +0300

    method add was added to OrderDaoImpl

--- src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -3,12 +3,15 @@ package mate.academy.dao.impl;
 import java.util.List;
 import mate.academy.dao.OrderDao;
 import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
 import mate.academy.model.Order;
 import mate.academy.model.User;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
+@Dao
 public class OrderDaoImpl implements OrderDao {
     @Override
     public List<Order> getByUser(User user) {
@@ -25,5 +28,27 @@ public class OrderDaoImpl implements OrderDao {
         } catch (Exception e) {
             throw new DataProcessingException("Can't fined orders in DB for user=" + user, e);
         }
+    }
+
+    @Override
+    public Order add(Order order) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(order);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add order to DB " + order, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return order;
     }
 }
