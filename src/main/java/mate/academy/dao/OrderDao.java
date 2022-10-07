808e67accd3747970e25fc2dad193ddb6b7237e2
Anatoliy Olkhovskiy <Anatoltbcm@gmail.com>
2022-10-07 19:35:34 +0300

    add orderDao

---
+++ src/main/java/mate/academy/dao/OrderDao.java
@@ -0,0 +1,7 @@
+package mate.academy.dao;
+
+import mate.academy.model.Order;
+
+public interface OrderDao {
+    Order add(Order order);
+}
---
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,32 @@
+package mate.academy.dao.impl;
+
+import mate.academy.dao.OrderDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.model.Order;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class OrderDaoImpl implements OrderDao {
+    @Override
+    public Order add(Order order) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(order);
+            transaction.commit();
+            return order;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add order: " + order, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
