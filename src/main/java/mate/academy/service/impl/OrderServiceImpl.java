ba1d8fb1bdd1b4274c099ccc43e767f79650f23c
Oleksandr-Tovstyi <raketaplaneta@gmail.com>
2022-10-07 14:09:31 +0300

    OrderServiceImpl was implemented

--- src/main/java/mate/academy/service/impl/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -1,6 +1,9 @@
 package mate.academy.service.impl;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import mate.academy.dao.OrderDao;
+import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.Order;
 import mate.academy.model.ShoppingCart;
@@ -9,13 +12,20 @@ import mate.academy.service.OrderService;
 
 @Service
 public class OrderServiceImpl implements OrderService {
+    @Inject
+    private OrderDao orderDao;
+
     @Override
     public Order completeOrder(ShoppingCart shoppingCart) {
-        return null;
+        Order order = new Order();
+        order.setUser(shoppingCart.getUser());
+        order.setTickets(shoppingCart.getTickets());
+        order.setOrderDate(LocalDateTime.now());
+        return orderDao.add(order);
     }
 
     @Override
     public List<Order> getOrdersHistory(User user) {
-        return null;
+        return orderDao.getByUser(user);
     }
 }
