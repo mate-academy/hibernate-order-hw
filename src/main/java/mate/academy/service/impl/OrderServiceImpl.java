d2f6d5675bd285baf898e6705510875e29d77b0b
Blokitin Oleh <87024861+oleh-satoshi@users.noreply.github.com>
2022-10-08 17:09:03 +0200

    implement OrderServiceImpl

---
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -0,0 +1,34 @@
+package mate.academy.service.impl;
+
+import mate.academy.dao.OrderDao;
+import mate.academy.lib.Inject;
+import mate.academy.model.Order;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import mate.academy.service.OrderService;
+import mate.academy.service.ShoppingCartService;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class OrderServiceImpl implements OrderService {
+    @Inject
+    private OrderDao orderDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
+
+    @Override
+    public Order completeOrder(ShoppingCart shoppingCart) {
+        Order order = new Order();
+        order.setOrderDate(LocalDateTime.now());
+        order.setUser(shoppingCart.getUser());
+        order.setTickets(shoppingCart.getTickets());
+        shoppingCartService.clearShoppingCart(shoppingCart);
+        return orderDao.add(order);
+    }
+
+    @Override
+    public List<Order> getOrdersHistory(User user) {
+        return orderDao.getOrderByUser(user);
+    }
+}
