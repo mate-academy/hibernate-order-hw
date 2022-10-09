fcb037893c8867c370b62c8f962f593829e3663c
Vasyl Pankiv <wasilius2@gmail.com>
2022-10-09 12:53:52 +0300

    fix code style

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -1,9 +1,18 @@
 package mate.academy.model;
 
-import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "orders")
