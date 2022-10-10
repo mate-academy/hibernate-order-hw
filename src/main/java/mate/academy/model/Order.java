3430ceb2a4a3226901d2f8dd312067df15946857
fliarkovskyi <fliarkovskyi@gmail.com>
2022-10-10 13:46:19 +0300

    fix checkstyle

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -59,6 +59,23 @@ public class Order {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Order order = (Order) o;
+        return Objects.equals(id, order.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
     public String toString() {
         return "Order{"
                 + "id=" + id
