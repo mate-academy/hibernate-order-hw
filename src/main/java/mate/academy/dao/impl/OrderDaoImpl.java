dc0a708732655b8524bb3ba595fa004c018b8394
LubusUzh <lubus.uzh@gmail.com>
2022-10-10 19:52:22 +0300

    Add: change joining type for the query in the method getByUser of the OrderDaoImpl class; use ArrayList constructor to set list of Tickets in the completeOrder method of the OrderServiceImpl class; add column name for orderDate in the Order class

--- src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -38,9 +38,10 @@ public class OrderDaoImpl implements OrderDao {
     public List<Order> getByUser(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             Query<Order> query = session.createQuery("FROM Order o "
-                    + "LEFT JOIN FETCH o.tickets t "
+                    + "JOIN FETCH o.tickets t "
                     + "LEFT JOIN FETCH o.user u "
                     + "LEFT JOIN FETCH t.movieSession ms "
+                    + "LEFT JOIN FETCH t.user "
                     + "LEFT JOIN FETCH ms.movie m "
                     + "LEFT JOIN FETCH ms.cinemaHall ch "
                     + "WHERE o.user =:user", Order.class);
