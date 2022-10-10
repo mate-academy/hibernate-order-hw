af36f0e6264143c07393e010b2c8628817667154
Aptypio13 <aptypio668@gmail.com>
2022-10-10 09:23:38 +0300

    Create Order model, order Dao and service with simple test in main

---
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,53 @@
+package mate.academy.dao.impl;
+
+import java.util.List;
+import mate.academy.dao.OrderDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Order;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class OrderDaoImpl implements OrderDao {
+    @Override
+    public Order add(Order order) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(order);
+            transaction.commit();
+        } catch (Exception exception) {
+            if (transaction != null) {
+                transaction.rollback();
+                throw new DataProcessingException("Can't insert a order: " + order, exception);
+            }
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+        return order;
+    }
+
+    @Override
+    public List<Order> getByUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from Order o"
+                            + " join fetch o.user "
+                            + " join fetch o.tickets t"
+                            + " join fetch t.movieSession ms"
+                            + " join fetch ms.cinemaHall"
+                            + " join fetch ms.movie"
+                            + " where o.user = :user", Order.class)
+                    .setParameter("user", user)
+                    .getResultList();
+        } catch (Exception exception) {
+            throw new DataProcessingException("Can`t find an order by user: " + user, exception);
+        }
+    }
+}
