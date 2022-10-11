9e6efcbe85a73775668c81587cede40451a926ea
Danylo Frolov <frolovdanilik@gmail.com>
2022-10-11 13:01:53 +0300

    applied changes

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -2,7 +2,16 @@ package mate.academy.model;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Order {
