3c19c45bba0d3d6933b0beece983f62b4837dc41
AntonPav <streetreadger@gmail.com>
2022-11-30 15:01:42 +0200

    Few corrections according to common mistakes

--- src/main/java/mate/academy/service/impl/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -9,19 +9,24 @@ import mate.academy.model.Order;
 import mate.academy.model.ShoppingCart;
 import mate.academy.model.User;
 import mate.academy.service.OrderService;
+import mate.academy.service.ShoppingCartService;
 
 @Service
 public class OrderServiceImpl implements OrderService {
     @Inject
     private OrderDao orderDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
 
     @Override
     public Order completeOrder(ShoppingCart shoppingCart) {
         Order order = new Order();
         order.setOrderDate(LocalDateTime.now());
         order.setUser(shoppingCart.getUser());
-        order.setTickets(shoppingCart.getTickets());
-        return orderDao.add(order);
+        order.setTickets(List.copyOf(shoppingCart.getTickets()));
+        order = orderDao.add(order);
+        shoppingCartService.clearShoppingCart(shoppingCart);
+        return order;
     }
 
     @Override
