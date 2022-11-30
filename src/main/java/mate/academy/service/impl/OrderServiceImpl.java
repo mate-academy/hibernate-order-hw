89b180897fc02a6ce1222e973fda9eac82746c94
Yura Hryniv <superyura444@gmail.com>
2022-11-30 17:29:15 +0200

    Used method clearShoppingCart() in completeOrder

--- src/main/java/mate/academy/service/impl/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import mate.academy.dao.OrderDao;
+import mate.academy.dao.ShoppingCartDao;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.Order;
@@ -11,17 +12,20 @@ import mate.academy.model.ShoppingCart;
 import mate.academy.model.Ticket;
 import mate.academy.model.User;
 import mate.academy.service.OrderService;
+import mate.academy.service.ShoppingCartService;
 
 @Service
 public class OrderServiceImpl implements OrderService {
     @Inject
     private OrderDao orderDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
 
     @Override
     public Order completeOrder(ShoppingCart shoppingCart) {
         List<Ticket> tickets = new ArrayList<>(shoppingCart.getTickets());
         User user = shoppingCart.getUser();
-        shoppingCart.getTickets().clear();
+        shoppingCartService.clearShoppingCart(shoppingCart);
         LocalDateTime localDateTime = LocalDateTime.now();
         return orderDao.add(new Order(tickets, localDateTime, user));
     }
