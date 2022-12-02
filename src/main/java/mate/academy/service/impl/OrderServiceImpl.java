ed92ad5071a14f03a1371ae14a9291caab397d94
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-12-03 00:09:15 +0200

    Create OrderService

---
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -0,0 +1,30 @@
+package mate.academy.service.impl;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import mate.academy.dao.OrderDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.Order;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import mate.academy.service.OrderService;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Inject
+    OrderDao orderDao;
+
+    @Override
+    public Order completeOrder(ShoppingCart shoppingCart) {
+        Order order = new Order(shoppingCart.getUser());
+        order.setTickets(shoppingCart.getTickets());
+        order.setOrderDate(LocalDateTime.now());
+        return orderDao.add(order);
+    }
+
+    @Override
+    public List<Order> getOrdersHistory(User user) {
+        return orderDao.getByUser(user);
+    }
+}
