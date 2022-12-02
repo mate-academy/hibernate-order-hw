baa42ca1b75d0ab69ccc014fe5c7eb762011025c
Anatolii Bespalko <tolyan9901@gmail.com>
2022-12-02 18:29:49 +0200

    Created or modification services

--- src/main/java/mate/academy/service/CinemaHallService.java
+++ src/main/java/mate/academy/service/CinemaHallService.java
@@ -3,9 +3,7 @@ package mate.academy.service;
 import java.util.List;
 import mate.academy.model.CinemaHall;
 
-public interface CinemaHallService {
-    CinemaHall add(CinemaHall cinemaHall);
-
+public interface CinemaHallService extends AbstractService<CinemaHall> {
     CinemaHall get(Long id);
     
     List<CinemaHall> getAll();
--- src/main/java/mate/academy/service/UserService.java
+++ src/main/java/mate/academy/service/UserService.java
@@ -3,8 +3,6 @@ package mate.academy.service;
 import java.util.Optional;
 import mate.academy.model.User;
 
-public interface UserService {
-    User add(User user);
-
+public interface UserService extends AbstractService<User> {
     Optional<User> findByEmail(String email);
 }
