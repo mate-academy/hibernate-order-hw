aec4efdf4c507f1d7c39884b82e4daca51e7bcde
Maksym Ivanov <ivanov1maxim@gmail.com>
2022-12-03 23:04:50 +0200

    Optimize code

--- src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -1,23 +1,15 @@
 package mate.academy.dao.impl;
 
 import java.util.List;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Fetch;
-import javax.persistence.criteria.JoinType;
-import javax.persistence.criteria.Root;
 import mate.academy.dao.OrderDao;
 import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
-import mate.academy.model.CinemaHall;
-import mate.academy.model.Movie;
-import mate.academy.model.MovieSession;
 import mate.academy.model.Order;
-import mate.academy.model.Ticket;
 import mate.academy.model.User;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
 @Dao
 public class OrderDaoImpl implements OrderDao {
@@ -30,13 +22,11 @@ public class OrderDaoImpl implements OrderDao {
             transaction = session.beginTransaction();
             session.save(order);
             transaction.commit();
-
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
             }
             throw new DataProcessingException("Can't save order " + order + " to DB.", e);
-
         } finally {
             if (session != null) {
                 session.close();
@@ -48,19 +38,16 @@ public class OrderDaoImpl implements OrderDao {
     @Override
     public List<Order> getByUser(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            CriteriaBuilder cb = session.getCriteriaBuilder();
-            CriteriaQuery<Order> query = cb.createQuery(Order.class);
-            Root<Order> root = query.from(Order.class);
-            Fetch<Order, User> orderUserFetch = root.fetch("user");
-            Fetch<Order, Ticket> orderTicketFetch = root.fetch("tickets", JoinType.LEFT);
-            Fetch<Ticket, MovieSession> ticketMovieSessionFetch
-                    = orderTicketFetch.fetch("movieSession", JoinType.LEFT);
-            Fetch<MovieSession, Movie> movieSessionMovieFetch
-                    = ticketMovieSessionFetch.fetch("movie", JoinType.LEFT);
-            Fetch<MovieSession, CinemaHall> movieSessionCinemaHallFetch
-                    = ticketMovieSessionFetch.fetch("cinemaHall", JoinType.LEFT);
-            query.where(cb.equal(root.get("user"), user));
-            return session.createQuery(query).getResultList();
+            Query<Order> query = session.createQuery(
+                    "FROM Order o "
+                            + "LEFT JOIN FETCH o.user "
+                            + "LEFT JOIN FETCH o.tickets t "
+                            + "LEFT JOIN FETCH t.movieSession ms "
+                            + "LEFT JOIN FETCH ms.movie "
+                            + "LEFT JOIN FETCH ms.cinemaHall "
+                            + "WHERE o.user = :user ", Order.class);
+            query.setParameter("user", user);
+            return query.getResultList();
         }
     }
 }
