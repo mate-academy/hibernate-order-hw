6b54892b82eb423bdfdabdc6021febf274e303d8
Maksym Shveda <mshveda28@gmail.com>
2022-12-05 19:30:48 +0200

    Replace custom constructor with lombok generated

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -14,28 +14,28 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 
 @Entity
 @Table(name = "orders")
 @Data
 @NoArgsConstructor
+@RequiredArgsConstructor
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NonNull
     private LocalDate orderDate;
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
+    @NonNull
     private User user;
     @OneToMany
     @JoinTable(name = "orders_tickets",
             joinColumns = @JoinColumn(name = "order_id"),
             inverseJoinColumns = @JoinColumn(name = "ticket_id"))
+    @NonNull
     private List<Ticket> tickets;
-
-    public Order(LocalDate orderDate, User user, List<Ticket> tickets) {
-        this.orderDate = orderDate;
-        this.user = user;
-        this.tickets = tickets;
-    }
 }
