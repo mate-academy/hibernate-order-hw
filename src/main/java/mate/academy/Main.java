c07a846fe3cb1b8eb9f6398ee4cda2ab09d15346
Artem Kurinniy <artem.kyrinniy@gmail.com>
2022-12-10 19:21:40 +0200

    added order feature to cinema project

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,55 +1,63 @@
 package mate.academy;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
 import mate.academy.model.MovieSession;
+import mate.academy.model.Order;
+import mate.academy.model.User;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
+import mate.academy.service.OrderService;
+import mate.academy.service.UserService;
 
 public class Main {
+    private static Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
-        MovieService movieService = null;
-
-        Movie fastAndFurious = new Movie("Fast and Furious");
-        fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
-        movieService.add(fastAndFurious);
-        System.out.println(movieService.get(fastAndFurious.getId()));
-        movieService.getAll().forEach(System.out::println);
-
-        CinemaHall firstCinemaHall = new CinemaHall();
-        firstCinemaHall.setCapacity(100);
-        firstCinemaHall.setDescription("first hall with capacity 100");
-
-        CinemaHall secondCinemaHall = new CinemaHall();
-        secondCinemaHall.setCapacity(200);
-        secondCinemaHall.setDescription("second hall with capacity 200");
-
-        CinemaHallService cinemaHallService = null;
-        cinemaHallService.add(firstCinemaHall);
-        cinemaHallService.add(secondCinemaHall);
-
-        System.out.println(cinemaHallService.getAll());
-        System.out.println(cinemaHallService.get(firstCinemaHall.getId()));
-
-        MovieSession tomorrowMovieSession = new MovieSession();
-        tomorrowMovieSession.setCinemaHall(firstCinemaHall);
-        tomorrowMovieSession.setMovie(fastAndFurious);
-        tomorrowMovieSession.setShowTime(LocalDateTime.now().plusDays(1L));
-
-        MovieSession yesterdayMovieSession = new MovieSession();
-        yesterdayMovieSession.setCinemaHall(firstCinemaHall);
-        yesterdayMovieSession.setMovie(fastAndFurious);
-        yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
-
-        MovieSessionService movieSessionService = null;
-        movieSessionService.add(tomorrowMovieSession);
-        movieSessionService.add(yesterdayMovieSession);
-
-        System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
-        System.out.println(movieSessionService.findAvailableSessions(
-                        fastAndFurious.getId(), LocalDate.now()));
+        DataGenerator dataGenerator;
+        MovieService movieService
+                = (MovieService) injector.getInstance(MovieService.class);
+        List<Movie> movieList = movieService.getAll();
+        System.out.println("Movies");
+        movieList.forEach(System.out::println);
+        System.out.println();
+        CinemaHallService cinemaHallService
+                = (CinemaHallService) injector.getInstance(CinemaHallService.class);
+        List<CinemaHall> cinemaHallList = cinemaHallService.getAll();
+        System.out.println("Cinema halls");
+        cinemaHallList.forEach(System.out::println);
+        System.out.println();
+        MovieSessionService movieSessionService
+                = (MovieSessionService) injector.getInstance(MovieSessionService.class);
+        System.out.println("Movie sessions on 12th November 2022");
+        for (long i = 1; i <= movieList.size(); i++) {
+            List<MovieSession> movieSessionList
+                    = movieSessionService.findAvailableSessions(
+                    i,
+                    LocalDate.of(2022, Month.NOVEMBER, 12));
+            movieSessionList.forEach(System.out::println);
+        }
+        System.out.println();
+        List<String> emails = DataGenerator.getUsersEmails();
+        List<User> users = new ArrayList<>();
+        UserService userService = (UserService) injector.getInstance(UserService.class);
+        for (String email : emails) {
+            users.add(userService.findByEmail(email).get());
+        }
+        OrderService orderService
+                = (OrderService) injector.getInstance(OrderService.class);
+        List<Order> orderList = new ArrayList<>();
+        for (User user : users) {
+            orderList.addAll(orderService.getOrdersHistory(user));
+        }
+        System.out.println("Order history of all users");
+        orderList.forEach(System.out::println);
     }
+
 }
