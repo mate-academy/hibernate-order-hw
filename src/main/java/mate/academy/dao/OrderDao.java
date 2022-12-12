19a37668f8c7c83d8f46e36219f8fe8962520556
Oleksiy Masaltsev <a.o.masaltsev@gmail.com>
2022-12-12 15:22:16 +0200

    shopping carts and tickets don't connect

---
+++ src/main/java/mate/academy/dao/OrderDao.java
@@ -0,0 +1,11 @@
+package mate.academy.dao;
+
+import mate.academy.model.Order;
+import mate.academy.model.User;
+
+import java.util.List;
+
+public interface OrderDao {
+   List<Order> getByUser(User user);
+   Order add(Order order);
+}
---
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,52 @@
+package mate.academy.dao.impl;
+
+import mate.academy.dao.OrderDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Order;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+@Dao
+public class OrderDaoImpl implements OrderDao {
+    public Order add(Order order) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(order);
+            transaction.commit();
+            return order;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add an order: " + order, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Order> getByUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Order> query = session.createQuery("from Order o " +
+                    "left join fetch o.tickets t " +
+                    "left join fetch t.movieSession ms " +
+                    "left join fetch ms.cinemaHall ch " +
+                    "left join fetch ms.movie m " +
+                    "where o.user = :user", Order.class);
+            return query.setParameter("user", user).getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get an order list for user: " + user, e);
+        }
+    }
+}
