243b7fccef780eea23c7dfd0482635810e06e20b
Oleksiy <a.o.masaltsev@gmail.com>
2022-12-13 12:00:42 +0200

    getByUser query with distinct select

--- src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -37,7 +37,8 @@ public class OrderDaoImpl implements OrderDao {
     @Override
     public List<Order> getByUser(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Query<Order> query = session.createQuery("from Order o "
+            Query<Order> query = session.createQuery(
+                    "select distinct o from Order o "
                     + "left join fetch o.tickets t "
                     + "left join fetch t.movieSession ms "
                     + "left join fetch ms.cinemaHall ch "
