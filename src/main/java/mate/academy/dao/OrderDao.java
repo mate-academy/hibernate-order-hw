6e9541166d430811c6016b858ae9d1d27932c5c8
Fokin <Fokin1@ukr.net>
2022-12-18 20:12:59 +0100

    create class Order in package model

---
+++ src/main/java/mate/academy/dao/OrderDao.java
@@ -0,0 +1,12 @@
+package mate.academy.dao;
+
+import java.util.List;
+import mate.academy.model.Order;
+import mate.academy.model.User;
+
+public interface OrderDao {
+
+    Order addOrder(Order order);
+
+    List<Order> getOrdersByUser(User user);
+}
