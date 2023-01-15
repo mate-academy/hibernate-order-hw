e11f6a96828f79e96b69250b4c190363a371d7fd
AndriySichniy <sichniy.andriy@gmail.com>
2023-01-15 15:59:45 +0200

    add OrderDao and OrderDaoImpl

---
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,47 @@
+package mate.academy.dao.impl;
+
+import mate.academy.dao.OrderDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.model.Order;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+public class OrderDaoImpl implements OrderDao {
+    @Override
+    public Order add(Order order) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(order);
+            transaction.commit();
+            return order;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add order" + order, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Order getByUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Order> getOrderByUserQuery = session.createQuery("from Order o"
+                    + " left join fetch o.user"
+                    + " where o.user = :user");
+            getOrderByUserQuery.setParameter("user", user);
+            return getOrderByUserQuery.uniqueResult();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get order by user: " + user, e);
+        }
+    }
+}
