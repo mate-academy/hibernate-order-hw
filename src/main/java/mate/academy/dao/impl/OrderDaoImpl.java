b83cbe907e2569ff1d695c9933c7094ab309e8b4
Maksim Surzhik <kabaneri15@gmail.com>
2023-02-10 10:25:02 +0200

    changes

--- src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -27,7 +27,7 @@ public class OrderDaoImpl implements OrderDao {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new DataProcessingException("Can't insert an order session: " + order, e);
+            throw new DataProcessingException("Can't insert an order: " + order, e);
         } finally {
             if (session != null) {
                 session.close();
