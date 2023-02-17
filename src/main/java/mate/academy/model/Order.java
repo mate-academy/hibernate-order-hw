6c92d8e56e23aa27d45e0921da1406feb2a4cb52
Inna Diachkina <innadyachkina@gmail.com>
2023-02-17 07:45:13 +0200

    fixed mistake

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -21,7 +21,7 @@ public class Order {
     private Long id;
     @OneToMany
     @JoinTable(name = "orders_tickets",
-            joinColumns = @JoinColumn(name = "orders_id"),
+            joinColumns = @JoinColumn(name = "order_id"),
             inverseJoinColumns = @JoinColumn(name = "ticket_id"))
     private List<Ticket> tickets;
     private LocalDateTime orderDate;
