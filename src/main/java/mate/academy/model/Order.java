63b546b52ecd04b4c319e85962e45483354b1db4
Oleksandr Kravchenko <oleksandr.kravchenko@spirent.com>
2023-03-06 18:01:25 +0200

    feat: hibernate-order comment was added Order Model

---
+++ src/main/java/mate/academy/model/Order.java
@@ -0,0 +1,78 @@
+package mate.academy.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(fetch = FetchType.LAZY)
+    private List<Ticket> tickets;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+    private LocalDateTime orderDate;
+
+    public Order(){
+    }
+
+    public Order(User user, List<Ticket> tickets, LocalDateTime orderDate) {
+        this.user = user;
+        this.tickets = tickets;
+        this.orderDate = orderDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{"
+                + "id=" + id
+                + ", tickets=" + tickets
+                + ", user=" + user
+                + ", orderDate=" + orderDate
+                + '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+}
