2c68f8ff579fd2b38a309d7d56851fb06b2e9fc9
Kateryna Takhtai <ktakhtay22@gmail.com>
2023-03-08 17:33:54 +0100

    Task hibernate-order is done.style changes

--- src/main/java/mate/academy/dao/impl/OrderDao.java
+++ src/main/java/mate/academy/dao/impl/OrderDao.java
@@ -3,6 +3,7 @@ package mate.academy.dao.impl;
 import java.util.List;
 import mate.academy.model.Order;
 import mate.academy.model.User;
+
 public interface OrderDao {
     Order createOrder(Order order);
 
