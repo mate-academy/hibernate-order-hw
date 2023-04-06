1a0164f47028e50e21d231d5b4afd866670461bd
Nazar Hnatyuk <108331415+Stim1@users.noreply.github.com>
2023-04-06 21:33:36 +0300

    hibernate-order-hw v 1.1 Added  FetchType.LAZY to @ManyToOne relationship.

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -2,13 +2,7 @@ package mate.academy.model;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "orders")
@@ -19,7 +13,7 @@ public class Order {
     @OneToMany
     private List<Ticket> tickets;
     private LocalDateTime orderDate;
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
     public Long getId() {
