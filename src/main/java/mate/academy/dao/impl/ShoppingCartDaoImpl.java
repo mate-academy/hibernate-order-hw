80cd8b90935076b95ef9b7d2c42877c691577f1c
Sergiy Golubchenko <galuba1@gmail.com>
2023-04-06 12:47:25 +0300

    Add Order model. Add Dao, Service layers

--- src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -8,7 +8,6 @@ import mate.academy.model.User;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import org.hibernate.query.Query;
 
 @Dao
 public class ShoppingCartDaoImpl implements ShoppingCartDao {
@@ -37,14 +36,14 @@ public class ShoppingCartDaoImpl implements ShoppingCartDao {
     @Override
     public ShoppingCart getByUser(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Query<ShoppingCart> query = session.createQuery("FROM ShoppingCart sc "
+            return session.createQuery("FROM ShoppingCart sc "
                     + "LEFT JOIN FETCH sc.tickets t "
                     + "LEFT JOIN FETCH t.movieSession ms "
                     + "LEFT JOIN FETCH ms.movie "
                     + "LEFT JOIN FETCH ms.cinemaHall "
-                    + "WHERE sc.user =:user", ShoppingCart.class);
-            query.setParameter("user", user);
-            return query.uniqueResult();
+                    + "WHERE sc.user =:user", ShoppingCart.class)
+                    .setParameter("user", user)
+                    .uniqueResult();
         } catch (Exception e) {
             throw new DataProcessingException("Can't find a shopping cart by user: " + user, e);
         }
