8070b4641b075f84cb5934717729ae9d4b6930ce
Nikita Melnikov <bam19921303@gmail.com>
2023-04-07 15:30:37 +0300

    fixed method completeOrder

--- src/main/java/mate/academy/service/impl/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package mate.academy.service.impl;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import mate.academy.dao.OrderDao;
 import mate.academy.lib.Inject;
@@ -9,21 +10,24 @@ import mate.academy.model.Order;
 import mate.academy.model.ShoppingCart;
 import mate.academy.model.User;
 import mate.academy.service.OrderService;
+import mate.academy.service.ShoppingCartService;
 
 @Service
 public class OrderServiceImpl implements OrderService {
-
     @Inject
     private OrderDao orderDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
 
     @Override
     public Order completeOrder(ShoppingCart shoppingCart) {
         Order order = new Order();
         order.setUser(shoppingCart.getUser());
-        order.setTickets(shoppingCart.getTickets());
+        order.setTickets(new ArrayList<>(shoppingCart.getTickets()));
         order.setOrderTime(LocalDateTime.now());
-        shoppingCart.getTickets().clear();
-        return orderDao.addOrder(order);
+        orderDao.addOrder(order);
+        shoppingCartService.clearShoppingCart(shoppingCart);
+        return order;
     }
 
     @Override
