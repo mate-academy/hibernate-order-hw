b666353df2869d91281399f6772d575197abea34
Yehor-Cherkashyn <gooooodvin1995@gmail.com>
2023-04-14 16:52:28 +0300

    Added order

--- src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/MovieSessionDaoImpl.java
@@ -28,7 +28,7 @@ public class MovieSessionDaoImpl implements MovieSessionDao {
         try {
             session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
-            session.save(movieSession);
+            session.persist(movieSession);
             transaction.commit();
             return movieSession;
         } catch (Exception e) {
