2da10fd722e6d47c889a396fcc59f63c5147bf84
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2023-04-19 19:56:38 +0300

    Order model was created, OrderDao and Service were implemented

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -21,8 +21,8 @@ public class Order {
     private Long id;
     @OneToMany
     @JoinTable(name = "orders_tickets", 
-        joinColumns = @JoinColumn(name = "order_id"), 
-        inverseJoinColumns = @JoinColumn(name = "ticket_id"))
+            joinColumns = @JoinColumn(name = "order_id"), 
+            inverseJoinColumns = @JoinColumn(name = "ticket_id"))
     private List<Ticket> tickets;
     private LocalDateTime orderDate;
     @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/mate/academy/service/impl/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -1,5 +1,6 @@
 package mate.academy.service.impl;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import mate.academy.dao.OrderDao;
 import mate.academy.lib.Inject;
@@ -8,28 +9,27 @@ import mate.academy.model.Order;
 import mate.academy.model.ShoppingCart;
 import mate.academy.model.User;
 import mate.academy.service.OrderService;
+import mate.academy.service.ShoppingCartService;
 
 @Service
 public class OrderServiceImpl implements OrderService {
     @Inject
-    OrderDao orderDao;
+    private OrderDao orderDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
 
     @Override
     public Order completeOrder(ShoppingCart shoppingCart) {
-//        orderDao.completeOrder(shoppingCart);
-        /*
-         * When passing tickets from shoppingCart to order — pass the copy of this
-         * list(you can use ArrayList constructor for this); The reason is that
-         * Hibernate will automatically delete these tickets from shoppingCart(in DB)
-         * after noticing them in our order object. Let’s do this manually using clear()
-         * method from ShoppingCartService after the order with tickets has been
-         * created.
-         */
-        return null;
+        Order order = new Order();
+        order.setOrderDate(LocalDateTime.now());
+        order.setUser(shoppingCart.getUser());
+        order.setTickets(List.copyOf(shoppingCart.getTickets()));
+        shoppingCartService.clearShoppingCart(shoppingCart);
+        return orderDao.add(order);
     }
 
     @Override
     public List<Order> getOrdersHistory(User user) {
-        return orderDao.getOrdersHistory(user);
+        return orderDao.getByUser(user);
     }
 }
