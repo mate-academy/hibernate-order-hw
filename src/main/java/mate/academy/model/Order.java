9b0f8173dc94924392e5094f4cfc72471400caf6
Nazar Zvarych <zvarych45@gmail.com>
2023-04-19 15:32:43 +0300

    I create model Order. I implement OrderDaoImpl, OrderServiceImpl and test it.

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -25,8 +25,10 @@ public class Order {
             joinColumns = @JoinColumn(name = "order_id"),
             inverseJoinColumns = @JoinColumn(name = "ticket_id"))
     private List<Ticket> tickets;
+    @JoinColumn(name = "order_date")
     private LocalDateTime orderDate;
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
     private User user;
 
     public Long getId() {
