71b5d5169eb28167b6d47be69773c7a482e4fbe5
Volodymyr Prysiazhniuk <verticalsyu@gmail.com>
2023-04-22 18:03:00 +0300

    Created model order and interfaces for orders

---
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -0,0 +1,19 @@
+package mate.academy.service.impl;
+
+import java.util.List;
+import mate.academy.model.Order;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import mate.academy.service.OrderService;
+
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public Order completeOrder(ShoppingCart shoppingCart) {
+        return null;
+    }
+
+    @Override
+    public List<Order> getOrdersHistory(User user) {
+        return null;
+    }
+}
