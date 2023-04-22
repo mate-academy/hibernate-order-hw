dffc1c0f935efe36eb194ec5e6e486bbe3defa34
copypasterr <114933128+copypasterr@users.noreply.github.com>
2023-04-22 22:28:40 +0300

    ready

--- src/main/java/mate/academy/dao/OrderDao.java
+++ src/main/java/mate/academy/dao/OrderDao.java
@@ -1,6 +1,12 @@
 package mate.academy.dao;
 
-public interface OrderDao {
+import java.util.List;
+import mate.academy.model.Order;
+import mate.academy.model.User;
 
+public interface OrderDao {
+    List<Order> getByUser(User user);
+
+    Order add(Order order);
 }
 
