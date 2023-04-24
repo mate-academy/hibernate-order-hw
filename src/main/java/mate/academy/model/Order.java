3336fdcde66ee91981c7c4452abc4381d06b8af6
Ilko Good <ilko.engineering@gmail.com>
2023-04-24 12:21:59 +0300

    mega fix

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -65,7 +65,6 @@ public class Order {
         return "Order{"
                 + "Id=" + id
                 + ", orderDate=" + orderDate
-                + ", tickets=" + tickets
                 + '}';
     }
 }
