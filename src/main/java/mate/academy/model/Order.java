0c7be9ad9463a3420bae7a8af1e75e9cb929d7f3
Ilko Good <ilko.engineering@gmail.com>
2023-04-24 11:13:31 +0300

    added checks for shopping cart

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -64,7 +64,6 @@ public class Order {
     public String toString() {
         return "Order{"
                 + "Id=" + id
-                + ", user=" + user
                 + ", orderDate=" + orderDate
                 + ", tickets=" + tickets
                 + '}';
--- src/main/java/mate/academy/service/impl/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -1,13 +1,14 @@
 package mate.academy.service.impl;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import mate.academy.dao.OrderDao;
+import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.Order;
 import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
 import mate.academy.model.User;
 import mate.academy.service.OrderService;
 import mate.academy.service.ShoppingCartService;
@@ -21,9 +22,16 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order completeOrder(ShoppingCart shoppingCart) {
+        if (shoppingCart == null) {
+            throw new DataProcessingException("Shopping cart is null, check system integrity");
+        }
         Order order = new Order();
         order.setUser(shoppingCart.getUser());
-        order.setTickets(new ArrayList<>(shoppingCart.getTickets()));
+        List<Ticket> tickets = shoppingCart.getTickets();
+        if (tickets.isEmpty()) {
+            throw new DataProcessingException("Can't complete order with zero tickets");
+        }
+        order.setTickets(tickets);
         order.setOrderDate(LocalDateTime.now());
         order = orderDao.add(order);
         shoppingCartService.clearShoppingCart(shoppingCart);
