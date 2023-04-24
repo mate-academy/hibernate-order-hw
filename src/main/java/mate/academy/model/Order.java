430c0b1d26a35efd9c5059bd9abd8f9c4e69a62b
Gleb Iaroshevych <randomeastecho@gmail.com>
2023-04-24 11:58:05 +0300

    Removed tickets from toString

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -67,7 +67,6 @@ public class Order {
     public String toString() {
         return "Order{"
                 + "id=" + id
-                + ", tickets=" + tickets
                 + ", orderDate=" + orderDate
                 + '}';
     }
