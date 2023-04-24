1282da9a383fe1d5fead42d99cd2d1065aa8306f
Nadiia Huryk <nadia11huryk@gmail.com>
2023-04-24 11:41:41 +0300

    Implement order

--- src/main/java/mate/academy/service/impl/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -4,7 +4,6 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import mate.academy.dao.OrderDao;
-import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
 import mate.academy.model.Order;
@@ -22,9 +21,9 @@ public class OrderServiceImpl implements OrderService {
 
     @Override
     public Order completeOrder(ShoppingCart shoppingCart) {
-        if (shoppingCart != null &&
-                shoppingCart.getTickets() != null &&
-                !shoppingCart.getTickets().isEmpty()) {
+        if (shoppingCart != null
+                && shoppingCart.getTickets() != null
+                && !shoppingCart.getTickets().isEmpty()) {
             Order order = new Order();
             order.setTickets(new ArrayList<>(shoppingCart.getTickets()));
             order.setOrderDate(LocalDateTime.now());
