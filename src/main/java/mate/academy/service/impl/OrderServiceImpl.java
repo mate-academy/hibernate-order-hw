f94bf275b00400f77df0866928f5d8b95297362a
Victoria Kharlanchuk <kaplabetty@gmail.com>
2023-05-04 12:05:02 +0200

    fixed checklist

--- src/main/java/mate/academy/service/impl/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -1,35 +1,31 @@
 package mate.academy.service.impl;
 
 import java.time.LocalDateTime;
-import java.util.Comparator;
+import java.util.ArrayList;
 import java.util.List;
 import mate.academy.dao.OrderDao;
 import mate.academy.lib.Inject;
 import mate.academy.lib.Service;
-import mate.academy.model.MovieSession;
 import mate.academy.model.Order;
 import mate.academy.model.ShoppingCart;
-import mate.academy.model.Ticket;
 import mate.academy.model.User;
 import mate.academy.service.OrderService;
+import mate.academy.service.ShoppingCartService;
 
 @Service
 public class OrderServiceImpl implements OrderService {
     @Inject
     private OrderDao orderDao;
+    @Inject
+    private ShoppingCartService shoppingCartService;
 
     @Override
     public Order completeOrder(ShoppingCart shoppingCart) {
         Order order = new Order();
-        order.setTickets(shoppingCart.getTickets());
+        order.setTickets(new ArrayList<>(shoppingCart.getTickets()));
         order.setUser(shoppingCart.getUser());
-        List<Ticket> tickets = shoppingCart.getTickets();
-        LocalDateTime lastDate = tickets.stream()
-                .map(Ticket::getMovieSession)
-                .map(MovieSession::getShowTime)
-                .max(Comparator.comparing(LocalDateTime::toLocalDate))
-                .orElse(null);
-        order.setOrderDate(lastDate);
+        order.setOrderDate(LocalDateTime.now());
+        shoppingCartService.clearShoppingCart(shoppingCart);
         return orderDao.add(order);
     }
 
