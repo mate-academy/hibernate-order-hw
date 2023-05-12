d583680e4a8ab690bde4622d41fec3874eae40ca
Artur Muliukov <arthur.muliukov@gmail.com>
2023-05-13 00:18:03 +0200

    Solved 1.1

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -7,6 +7,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.MapsId;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -22,6 +23,7 @@ public class Order {
     private LocalDateTime orderDate;
     @OneToOne
     @JoinColumn(name = "user_id")
+    @MapsId
     private User user;
 
     public Order() {
