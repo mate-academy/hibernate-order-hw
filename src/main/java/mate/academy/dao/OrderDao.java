35428b0b93f5624ec4ee5754521fa436458ff598
Gamemod13 <igorfetov1997@gmail.com>
2023-07-10 19:36:05 +0300

    Create structure of task implementation

--- src/main/java/mate/academy/dao/OrderDao.java
+++ src/main/java/mate/academy/dao/OrderDao.java
@@ -1,15 +1,14 @@
 package mate.academy.dao;
 
+import java.util.List;
 import java.util.Optional;
-import mate.academy.lib.Dao;
 import mate.academy.model.Order;
 import mate.academy.model.User;
 
-@Dao
 public interface OrderDao {
-    Optional<Order> getByUser(User user);
+    Optional<List<Order>> getByUser(User user);
 
-    Optional<Order> get(Long id);
+    Optional<List<Order>> get(Long id);
 
-    Order add(Order session);
+    Order add(Order order);
 }
--- src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -1,24 +1,74 @@
 package mate.academy.dao.impl;
 
+import java.util.List;
 import java.util.Optional;
 import mate.academy.dao.OrderDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
 import mate.academy.model.Order;
 import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
+@Dao
 public class OrderDaoImpl implements OrderDao {
     @Override
-    public Optional<Order> getByUser(User user) {
-
-        return Optional.empty();
+    public Optional<List<Order>> getByUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Order> getByUserQuery = session.createQuery("FROM Order o "
+                                                              + "LEFT JOIN FETCH o.tickets t "
+                                                              + "LEFT JOIN FETCH t.movieSession ms "
+                                                              + "LEFT JOIN FETCH ms.movie m"
+                                                              + "LEFT JOIN FETCH ms.cinemaHall ch"
+                                                              + "LEFT JOIN FETCH t.user tu "
+                                                              + "LEFT JOIN FETCH o.user u "
+                                                              + "WHERE u = :user", Order.class);
+            getByUserQuery.setParameter("user", user);
+            return Optional.ofNullable(getByUserQuery.getResultList());
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get Order by User: " + user, e);
+        }
     }
 
     @Override
-    public Optional<Order> get(Long id) {
-        return Optional.empty();
+    public Optional<List<Order>> get(Long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Query<Order> getByUserQuery = session.createQuery("FROM Order o "
+                                                              + "LEFT JOIN FETCH o.tickets t "
+                                                              + "LEFT JOIN FETCH t.movieSession ms "
+                                                              + "LEFT JOIN FETCH ms.movie m"
+                                                              + "LEFT JOIN FETCH ms.cinemaHall ch"
+                                                              + "LEFT JOIN FETCH t.user tu "
+                                                              + "LEFT JOIN FETCH o.user u "
+                                                              + "WHERE o.id = :id", Order.class);
+            getByUserQuery.setParameter("id", id);
+            return Optional.ofNullable(getByUserQuery.getResultList());
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get Order by ID: " + id, e);
+        }
     }
 
     @Override
-    public Order add(Order session) {
-        return null;
+    public Order add(Order order) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(order);
+            transaction.commit();
+            return order;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add order: " + order + " to DB.", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 }
