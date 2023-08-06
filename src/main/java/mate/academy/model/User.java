1e9c3a6440c9593e55356e544d39d3ed8557943c
Bohdan Kaziamyr <notpadumink01@gmail.com>
2023-08-06 14:13:38 +0300

    added solution

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -53,8 +53,8 @@ public class User {
     @Override
     public String toString() {
         return "User{"
-            + "id=" + id
-            + ", email='" + email + '\''
-            + '}';
+                + "id=" + id
+                + ", email='" + email + '\''
+                + '}';
     }
 }
