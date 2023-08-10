577d5f23216a78e8b5d110930022b9e0dd93ed1f
Anton Uzhva <93510735+AntonU91@users.noreply.github.com>
2023-08-10 16:38:46 +0300

    Update src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
    Commit mentor correcting
    
    Co-authored-by: Oleksandr K. <overpathz@gmail.com>

--- src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -38,10 +38,14 @@ public class OrderDaoImpl implements OrderDao {
     @Override
     public List<Order> getByUser(User user) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            Query<Order> query = session.createQuery("FROM Order o LEFT JOIN FETCH o.tickets "
-                                                     + "LEFT JOIN FETCH o.user "
-                                                     + "WHERE o.user.id = :id", Order.class);
-            query.setParameter("id", user.getId());
+Query<Order> query = session.createQuery("FROM Order o "
+                    + "LEFT JOIN FETCH o.user "
+                    + "LEFT JOIN FETCH o.tickets t "
+                    + "LEFT JOIN FETCH t.movieSession ms "
+                    + "LEFT JOIN FETCH ms.movie "
+                    + "LEFT JOIN FETCH ms.cinemaHall "
+                    + "WHERE o.user =:user", Order.class);
+            query.setParameter("user", user);
             return query.getResultList();
         } catch (Exception e) {
             throw new DataProcessingException("Can't get orders by user: " + user, e);
