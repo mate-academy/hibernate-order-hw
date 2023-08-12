fa9cd7aaf93681224512896bf89d3cef30eef5b9
Roman Kuchmii <romakuchmii@gmail.com>
2023-08-12 16:14:14 +0300

    added solution

--- src/main/java/mate/academy/model/User.java
+++ src/main/java/mate/academy/model/User.java
@@ -13,7 +13,7 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(unique = true)
+    @Column(unique = true, length = 128)
     private String email;
     private String password;
     private byte[] salt;
