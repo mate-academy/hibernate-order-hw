370019c70f80e1ef417effeccaa0f6b61a385cda
literandltx <romanskovronn@gmail.com>
2023-08-22 22:15:25 +0300

    fix

--- src/main/java/mate/academy/service/impl/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -5,12 +5,14 @@ import java.util.ArrayList;
 import java.util.List;
 import mate.academy.dao.OrderDao;
 import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
 import mate.academy.model.Order;
 import mate.academy.model.ShoppingCart;
 import mate.academy.model.User;
 import mate.academy.service.OrderService;
 import mate.academy.service.ShoppingCartService;
 
+@Service
 public class OrderServiceImpl implements OrderService {
     @Inject
     private OrderDao orderDao;
