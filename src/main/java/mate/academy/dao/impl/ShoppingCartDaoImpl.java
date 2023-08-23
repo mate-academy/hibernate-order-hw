7b40a4bc217c8c1266ab7ad0fae5261af3d5af2a
Zhdanov Anton <anton1992xxxx@gmail.com>
2023-08-24 01:50:03 +0300

    hw-hibernate-order

--- src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -19,7 +19,7 @@ public class ShoppingCartDaoImpl implements ShoppingCartDao {
         try {
             session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
-            transaction.begin();
+            shoppingCart.setUser(session.merge(shoppingCart.getUser()));
             session.persist(shoppingCart);
             transaction.commit();
             return shoppingCart;
@@ -58,7 +58,7 @@ public class ShoppingCartDaoImpl implements ShoppingCartDao {
         try {
             session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
-            session.update(shoppingCart);
+            session.merge(shoppingCart);
             transaction.commit();
         } catch (Exception e) {
             if (transaction != null) {
