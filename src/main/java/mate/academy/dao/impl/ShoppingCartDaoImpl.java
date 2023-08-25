0fbcd573960589ce1f9f153d44946b30bc21e50d
Dmytro Varukha <dmytro.varukha@gmail.com>
2023-08-25 11:57:39 +0300

    changed ShoppingCart add(ShoppingCart shoppingCart) method

--- src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -19,9 +19,9 @@ public class ShoppingCartDaoImpl implements ShoppingCartDao {
         try {
             session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
-            session.persist(shoppingCart);
             User managedUser = session.merge(shoppingCart.getUser());
             shoppingCart.setUser(managedUser);
+            session.persist(shoppingCart);
             transaction.commit();
             return shoppingCart;
         } catch (Exception e) {
