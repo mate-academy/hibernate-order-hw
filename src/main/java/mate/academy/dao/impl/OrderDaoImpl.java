00d79e8b327e65f1a988c0c2c08f109b88881cb1
Tim <tymurprokhorov@gmail.com>
2023-09-06 12:47:30 +0300

    Order model created, orderDao and service implemented

---
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,58 @@
+package mate.academy.dao.impl;
+
+import java.util.List;
+import mate.academy.dao.OrderDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Order;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+@Dao
+public class OrderDaoImpl implements OrderDao {
+    public static final SessionFactory FACTORY = HibernateUtil.getSessionFactory();
+
+    @Override
+    public Order add(Order order) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = FACTORY.openSession();
+            transaction = session.beginTransaction();
+            session.save(order);
+            transaction.commit();
+            return order;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't add order: " + order, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Order> getByUser(User user) {
+        try (Session session = FACTORY.openSession()) {
+            Query<Order> getOrderByUserQuery = session.createQuery(
+                    "FROM Order o "
+                            + "LEFT JOIN FETCH o.tickets t "
+                            + "LEFT JOIN FETCH t.movieSession ms "
+                            + "LEFT JOIN FETCH ms.cinemaHall "
+                            + "LEFT JOIN FETCH ms.movie "
+                            + "LEFT JOIN FETCH t.user "
+                            + "WHERE o.user = :user", Order.class);
+            getOrderByUserQuery.setParameter("user", user);
+            return getOrderByUserQuery.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can`t get order by user: " + user, e);
+        }
+    }
+}
