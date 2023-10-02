4107fb24fef3fc7dc77b3637e3953a81687a0555
Maksym Fedorenko <fedorenko.max02@gmail.com>
2023-10-02 12:09:12 +0300

    added few fixes

--- src/main/java/mate/academy/dao/impl/AbstractDao.java
+++ src/main/java/mate/academy/dao/impl/AbstractDao.java
@@ -3,13 +3,33 @@ package mate.academy.dao.impl;
 import java.util.function.Function;
 import mate.academy.util.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 public class AbstractDao {
     protected <T> T performReturningWithinTx(Function<Session, T> sessionFunction) {
-        return HibernateUtil.getSessionFactory().fromTransaction(sessionFunction);
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            T result = sessionFunction.apply(session);
+            transaction.commit();
+            return result;
+        } catch (Exception exception) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw exception;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
     }
 
     protected <T> T performReturningWithoutTx(Function<Session, T> sessionFunction) {
-        return HibernateUtil.getSessionFactory().fromSession(sessionFunction);
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return sessionFunction.apply(session);
+        }
     }
 }
