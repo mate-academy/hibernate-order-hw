fdec86752298ab43d0b2c1f7407b862c2518d9f8
Olena <yelenastankevych@gmail.com>
2023-10-05 11:38:18 +0300

    added fixes after mentors review

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -64,13 +64,4 @@ public class Order {
     public void setUser(User user) {
         this.user = user;
     }
-
-    @Override
-    public String toString() {
-        return "Order{"
-                + "id=" + id
-                + ", orderDate=" + orderDate
-                + ", user=" + user
-                + '}';
-    }
 }
