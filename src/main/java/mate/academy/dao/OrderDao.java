9d0c7d367ffe89383f11a5c14a4859eb27aba528
Anatolii Nazarov <anatoly_n@ukr.net>
2023-11-07 16:07:44 +0200

    For check

---
+++ src/main/java/mate/academy/dao/OrderDao.java
@@ -0,0 +1,14 @@
+package mate.academy.dao;
+
+import mate.academy.model.Order;
+import mate.academy.model.User;
+
+import java.util.List;
+
+public interface OrderDao {
+    Order add(Order order);
+
+    List<Order> getByUser(User user);
+
+    List<Order> getAll();
+}
