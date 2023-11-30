0e9f98edae6208924d2a0e93826c93d99f51f968
Nataliia <gzodchie@gmail.com>
2023-11-30 12:51:30 +0200

    fixed Optional in service package

--- src/main/java/mate/academy/service/MovieService.java
+++ src/main/java/mate/academy/service/MovieService.java
@@ -1,12 +1,13 @@
 package mate.academy.service;
 
 import java.util.List;
+import java.util.Optional;
 import mate.academy.model.Movie;
 
 public interface MovieService {
     Movie add(Movie movie);
 
-    Movie get(Long id);
+    Optional<Movie> get(Long id);
 
     List<Movie> getAll();
 }
