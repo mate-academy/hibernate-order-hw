6b59cc6c2fa1f2613855353ce297774c51c066c2
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-02 23:53:23 +0200

    created OrderDao interface

---
+++ src/main/java/mate/academy/dao/OrderDao.java
@@ -0,0 +1,9 @@
+package mate.academy.dao;
+
+import mate.academy.model.Order;
+import mate.academy.model.User;
+import java.util.List;
+
+public interface OrderDao {
+    List<Order> getByUser(User user);
+}
