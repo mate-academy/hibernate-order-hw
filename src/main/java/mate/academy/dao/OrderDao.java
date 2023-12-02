d7e992827da000f2612933e33f41ae9679bc8211
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-02 23:55:07 +0200

    fixed checkstyle

--- src/main/java/mate/academy/dao/OrderDao.java
+++ src/main/java/mate/academy/dao/OrderDao.java
@@ -1,8 +1,8 @@
 package mate.academy.dao;
 
+import java.util.List;
 import mate.academy.model.Order;
 import mate.academy.model.User;
-import java.util.List;
 
 public interface OrderDao {
     List<Order> getByUser(User user);
