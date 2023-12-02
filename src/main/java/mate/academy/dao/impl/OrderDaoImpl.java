175b96728ccf735c452cf9949b0ae749228ceeda
Valerii Fedorovych <valera.fedorovich.2000@gmail.com>
2023-12-03 00:12:45 +0200

    created OrderDaoImpl

---
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,29 @@
+package mate.academy.dao.impl;
+
+import java.util.List;
+import mate.academy.dao.OrderDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Order;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+
+@Dao
+public class OrderDaoImpl implements OrderDao {
+    private final SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+
+    @Override
+    public List<Order> getByUser(User user) {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Order> query = session.createQuery("FROM Order o "
+                    + "WHERE o.user =:user", Order.class);
+            query.setParameter("user", user);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't find an order by user: " + user, e);
+        }
+    }
+}
