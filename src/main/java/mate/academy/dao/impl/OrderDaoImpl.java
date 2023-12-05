8928b6c98de9d35ebe4efd79f0b3c398d2329703
Andriy-Hrynevetskyi <andrijgrineveckij@gmail.com>
2023-12-05 19:18:00 +0200

    added order order dao and service

---
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,17 @@
+package mate.academy.dao.impl;
+
+import mate.academy.dao.OrderDao;
+import mate.academy.model.Order;
+import mate.academy.model.User;
+
+public class OrderDaoImpl implements OrderDao {
+    @Override
+    public Order add(Order order) {
+        return null;
+    }
+
+    @Override
+    public Order getByUser(User user) {
+        return null;
+    }
+}
