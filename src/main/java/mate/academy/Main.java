693648c41a1a8e57632df1dbe79ec6ce181f477b
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2023-12-31 15:47:18 +0200

    added changes to the class Main

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -19,6 +19,7 @@ import mate.academy.service.ShoppingCartService;
 
 public class Main {
     private static final Injector injector = Injector.getInstance("mate.academy");
+
     public static void main(String[] args) {
         MovieService movieService
                 = (MovieService) injector.getInstance(MovieService.class);
