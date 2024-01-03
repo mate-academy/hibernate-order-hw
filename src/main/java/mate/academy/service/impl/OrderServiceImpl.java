c607949d37640e473a8ad68849cce34f2b2a9969
Kirkir777 <106085319+Kirkir777@users.noreply.github.com>
2024-01-03 14:52:13 +0200

    added changes to the class OrderServiceImpl

--- src/main/java/mate/academy/service/impl/OrderServiceImpl.java
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -1,6 +1,7 @@
 package mate.academy.service.impl;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import mate.academy.dao.OrderDao;
 import mate.academy.lib.Inject;
@@ -10,20 +11,26 @@ import mate.academy.model.ShoppingCart;
 import mate.academy.model.Ticket;
 import mate.academy.model.User;
 import mate.academy.service.OrderService;
+import mate.academy.service.ShoppingCartService;
 
 @Service
 public class OrderServiceImpl implements OrderService {
     @Inject
     private OrderDao orderDao;
 
+    @Inject
+    private ShoppingCartService shoppingCartService;
+
     @Override
     public Order completeOrder(ShoppingCart shoppingCart) {
-        List<Ticket> tickets = shoppingCart.getTickets();
+        List<Ticket> tickets = new ArrayList<>(shoppingCart.getTickets());
         Order order = new Order();
         order.setTickets(tickets);
         order.setUser(shoppingCart.getUser());
         order.setOrderDate(LocalDateTime.now());
-        return orderDao.add(order);
+        Order createdOrder = orderDao.add(order);
+        shoppingCartService.clearShoppingCart(shoppingCart);
+        return createdOrder;
     }
 
     @Override
