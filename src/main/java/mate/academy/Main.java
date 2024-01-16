4669d9be7622e3fe0d96a981613b22e4185dac4a
Lukian Muzyka <lukianmuzyka@gmail.com>
2024-01-16 14:51:13 +0200

    Created instance of OrderService using injector and test all methods from Main

--- src/main/java/mate/academy/Main.java
+++ src/main/java/mate/academy/Main.java
@@ -1,18 +1,31 @@
 package mate.academy;
 
+import java.util.List;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.dao.TicketDao;
+import mate.academy.dao.UserDao;
+import mate.academy.dao.impl.ShoppingCartDaoImpl;
+import mate.academy.dao.impl.TicketDaoImpl;
+import mate.academy.dao.impl.UserDaoImpl;
+import mate.academy.lib.Injector;
 import mate.academy.model.CinemaHall;
 import mate.academy.model.Movie;
 import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
+import mate.academy.model.User;
 import mate.academy.service.CinemaHallService;
 import mate.academy.service.MovieService;
 import mate.academy.service.MovieSessionService;
+import mate.academy.service.OrderService;
 
 public class Main {
-    public static void main(String[] args) {
-        MovieService movieService = null;
+    private static final Injector injector = Injector.getInstance("mate.academy");
 
+    public static void main(String[] args) {
+        MovieService movieService = (MovieService) injector.getInstance(MovieService.class);
         Movie fastAndFurious = new Movie("Fast and Furious");
         fastAndFurious.setDescription("An action film about street racing, heists, and spies.");
         movieService.add(fastAndFurious);
@@ -27,8 +40,8 @@ public class Main {
         secondCinemaHall.setCapacity(200);
         secondCinemaHall.setDescription("second hall with capacity 200");
 
-        CinemaHallService cinemaHallService = null;
-        cinemaHallService.add(firstCinemaHall);
+        CinemaHallService cinemaHallService = (CinemaHallService) injector
+                .getInstance(CinemaHallService.class);        cinemaHallService.add(firstCinemaHall);
         cinemaHallService.add(secondCinemaHall);
 
         System.out.println(cinemaHallService.getAll());
@@ -44,12 +57,42 @@ public class Main {
         yesterdayMovieSession.setMovie(fastAndFurious);
         yesterdayMovieSession.setShowTime(LocalDateTime.now().minusDays(1L));
 
-        MovieSessionService movieSessionService = null;
+        MovieSessionService movieSessionService = (MovieSessionService) injector
+                .getInstance(MovieSessionService.class);
         movieSessionService.add(tomorrowMovieSession);
         movieSessionService.add(yesterdayMovieSession);
 
         System.out.println(movieSessionService.get(yesterdayMovieSession.getId()));
         System.out.println(movieSessionService.findAvailableSessions(
-                        fastAndFurious.getId(), LocalDate.now()));
+                fastAndFurious.getId(), LocalDate.now()));
+
+        OrderService orderService = (OrderService) injector
+                .getInstance(OrderService.class);
+
+        User testUser = new User();
+        testUser.setEmail("email@gmail.com");
+        testUser.setPassword("qwerty123");
+        UserDao userDao = new UserDaoImpl();
+        userDao.add(testUser);
+
+        Ticket testTicket = new Ticket();
+        testTicket.setUser(testUser);
+        testTicket.setMovieSession(tomorrowMovieSession);
+        TicketDao ticketDao = new TicketDaoImpl();
+        ticketDao.add(testTicket);
+
+        Ticket testTicket1 = new Ticket();
+        testTicket1.setUser(testUser);
+        testTicket1.setMovieSession(tomorrowMovieSession);
+        ticketDao.add(testTicket1);
+
+        ShoppingCart testShoppingCart = new ShoppingCart();
+        testShoppingCart.setUser(testUser);
+        testShoppingCart.setTickets(List.of(testTicket, testTicket1));
+        ShoppingCartDao shoppingCartDao = new ShoppingCartDaoImpl();
+        shoppingCartDao.add(testShoppingCart);
+
+        orderService.completeOrder(testShoppingCart);
+        orderService.getOrdersHistory(testUser);
     }
 }
