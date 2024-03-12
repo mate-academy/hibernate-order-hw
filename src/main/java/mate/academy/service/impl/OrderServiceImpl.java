9a951aa8e168fae1371a9764eac66fa4dab4faf7
Oleh <mihailyakyb45@gmail.com>
2024-03-12 22:56:22 +0200

    done

---
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -0,0 +1,29 @@
+package mate.academy.service.impl;
+
+import java.util.List;
+import mate.academy.dao.OrderDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.Order;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import mate.academy.service.OrderService;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Inject
+    private OrderDao orderDao;
+
+    @Override
+    public Order completeOrder(ShoppingCart shoppingCart) {
+        Order order = new Order();
+        order.setUser(shoppingCart.getUser());
+        order.setTickets(List.copyOf(shoppingCart.getTickets()));
+        return orderDao.add(order);
+    }
+
+    @Override
+    public List<Order> getOrdersHistory(User user) {
+        return orderDao.getOrdersHistory(user);
+    }
+}
