8c529d72605408d4d5be14515b026d8bf630f83a
Evgeniy Chernonog <chernonog.evgeniy@gmail.com>
2024-03-24 09:34:26 +0200

    added fixes after st_review

--- src/main/java/mate/academy/dao/OrderDao.java
+++ src/main/java/mate/academy/dao/OrderDao.java
@@ -1,7 +1,6 @@
 package mate.academy.dao;
 
 import java.util.List;
-import java.util.Optional;
 import mate.academy.model.Order;
 import mate.academy.model.User;
 
--- src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -1,7 +1,6 @@
 package mate.academy.dao.impl;
 
 import java.util.List;
-import java.util.Optional;
 import mate.academy.dao.OrderDao;
 import mate.academy.exception.DataProcessingException;
 import mate.academy.lib.Dao;
