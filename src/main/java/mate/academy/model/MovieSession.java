d9b002406ced2628b3ec7576e9e2b40acc2b1982
Liza <lizabalashova7@gmail.com>
2024-04-07 13:08:32 +0300

    add order

--- src/main/java/mate/academy/model/MovieSession.java
+++ src/main/java/mate/academy/model/MovieSession.java
@@ -20,9 +20,9 @@ public class MovieSession {
     @ManyToOne(fetch = FetchType.LAZY)
     private Movie movie;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "cinema_hall_id")
+    @JoinColumn(name = "cinemaHall_id")
     private CinemaHall cinemaHall;
-    @Column(name = "show_time")
+    @Column(name = "showTime")
     private LocalDateTime showTime;
 
     public Long getId() {
