7b1b428a775627f6a68803796b297f3e554e3566
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-04-08 17:18:47 +0300

    added name for column

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -19,6 +20,7 @@ public class Order {
     @OneToMany
     private List<Ticket> ticketList;
     @OneToOne
+    @JoinColumn(name = "user_id")
     private User user;
     private LocalDateTime orderTime;
 
