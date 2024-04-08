398682a57544f0080f2d1f1fc9c328beeb297f04
Dmytro Hadiuchko <hadiuchko.dmytro@gmail.com>
2024-04-08 17:09:08 +0300

    implementation was added

---
+++ src/main/java/mate/academy/model/Order.java
@@ -0,0 +1,70 @@
+package mate.academy.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToMany
+    private List<Ticket> ticketList;
+    @OneToOne
+    private User user;
+    private LocalDateTime orderTime;
+
+    public Order() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Ticket> getTicketList() {
+        return ticketList;
+    }
+
+    public void setTicketList(List<Ticket> ticketList) {
+        this.ticketList = ticketList;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDateTime getOrderTime() {
+        return orderTime;
+    }
+
+    public void setOrderTime(LocalDateTime orderTime) {
+        this.orderTime = orderTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{"
+                + "id=" + id
+                + ", ticketList="
+                + ticketList
+                + ", user=" + user
+                + ", orderTime=" + orderTime
+                + '}';
+    }
+}
