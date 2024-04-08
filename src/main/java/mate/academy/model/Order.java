ff7f2754264c3496eb613f40c20ebd433f85f1ca
makaroshyna <katya23225@gmail.com>
2024-04-08 18:50:31 +0300

    Change join table -> join column.

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -20,9 +20,7 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @OneToMany
-    @JoinTable(name = "orders_tickets",
-            joinColumns = @JoinColumn(name = "order_id"),
-            inverseJoinColumns = @JoinColumn(name = "ticket_id"))
+    @JoinColumn(name = "tickets_id")
     private List<Ticket> tickets;
     @CreationTimestamp
     private LocalDateTime orderDate;
