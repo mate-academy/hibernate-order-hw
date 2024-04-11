04335886546be3168d8e7840adaa506571a2d6d1
Valentyn Sharshon <woosay333@gmail.com>
2024-04-11 15:45:32 +0300

    created Order entity class

---
+++ src/main/java/mate/academy/model/Order.java
@@ -0,0 +1,78 @@
+package mate.academy.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany
+    private List<Ticket> tickets;
+
+    private LocalDateTime orderDateTime;
+
+    @ManyToOne
+    private User user;
+
+    public Order() {
+    }
+
+    public Order(List<Ticket> tickets, LocalDateTime orderDateTime, User user) {
+        this.tickets = tickets;
+        this.orderDateTime = orderDateTime;
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Ticket> getTickets() {
+        return tickets;
+    }
+
+    public void setTickets(List<Ticket> tickets) {
+        this.tickets = tickets;
+    }
+
+    public LocalDateTime getOrderDateTime() {
+        return orderDateTime;
+    }
+
+    public void setOrderDateTime(LocalDateTime orderDateTime) {
+        this.orderDateTime = orderDateTime;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", tickets=" + tickets +
+                ", orderDateTime=" + orderDateTime +
+                ", user=" + user +
+                '}';
+    }
+}
