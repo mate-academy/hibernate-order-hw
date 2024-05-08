e609ed9d01f7a4bc637b1d98eb7a64ee3e2cafe8
Nikita <nik.pogrebnoy@gmail.com>
2024-05-08 20:25:00 +0300

    Implemented order

--- src/main/java/mate/academy/model/Order.java
+++ src/main/java/mate/academy/model/Order.java
@@ -1,6 +1,5 @@
 package mate.academy.model;
 
-import java.time.LocalDateTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -11,6 +10,7 @@ import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
