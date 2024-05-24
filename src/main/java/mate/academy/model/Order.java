504dc4df47108bdb720abfa226546d3ead8855c7
Anton <StarAntonU@gmail.com>
2024-05-24 23:27:57 +0300

    create class Order

---
+++ src/main/java/mate/academy/model/Order.java
@@ -0,0 +1,18 @@
+package mate.academy.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToMany
+    private List<Ticket> tickets;
+    private LocalDateTime orderDate;
+    @ManyToOne
+    private User user;
+}
