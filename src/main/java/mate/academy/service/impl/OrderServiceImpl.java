f1d78fd9e138397d3d5e089fa6d5565d1d4cac8d
NekitCool1 <gnnikita97@gmail.com>
2024-06-14 12:54:16 +0300

    The task has done according 'README' file

---
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -0,0 +1,34 @@
+package mate.academy.service.impl;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import mate.academy.dao.OrderDao;
+import mate.academy.lib.Inject;
+import mate.academy.lib.Service;
+import mate.academy.model.Order;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import mate.academy.service.OrderService;
+import mate.academy.service.ShoppingCartService;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Inject
+    private OrderDao orderDao;
+    private ShoppingCartService shoppingCartDao = new ShoppingCartServiceImpl();
+
+    @Override
+    public Order completeOrder(ShoppingCart shoppingCart) {
+        Order order = new Order();
+        order.setTickets(shoppingCart.getTickets());
+        order.setOrderDate(LocalDateTime.now());
+        order.setUser(shoppingCart.getUser());
+        orderDao.add(order);
+        return order;
+    }
+
+    @Override
+    public List<Order> getOrdersHistory(User user) {
+        return orderDao.getOrdersHistory(user);
+    }
+}
