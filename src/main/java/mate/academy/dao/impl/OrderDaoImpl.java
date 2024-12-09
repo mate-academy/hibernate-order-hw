4186ad5715317b5afcdb11001b6f0fe1d302cfa2
smagles <lesialist@icloud.com>
2024-12-09 21:12:40 +0100

    Implement order

---
+++ src/main/java/mate/academy/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,56 @@
+package mate.academy.dao.impl;
+
+import java.util.List;
+import mate.academy.dao.OrderDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Order;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+@Dao
+public class OrderDaoImpl implements OrderDao {
+
+    @Override
+    public Order add(Order order) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.persist(order);
+            transaction.commit();
+            return order;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert a order: "
+                    + order, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<Order> getByUser(User user) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("from Order o "
+                            + "left join fetch o.tickets t "
+                            + "left join fetch o.user u "
+                            + "left join fetch t.movieSession ms "
+                            + "left join fetch ms.movie "
+                            + "where o.user = :user", Order.class)
+                    .setParameter("user", user)
+                    .list();
+        } catch (Exception e) {
+            throw new DataProcessingException(
+                    "Can't get orders by user: "
+                            + user, e);
+        }
+    }
+}
