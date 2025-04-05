d2f85837a10975ff8f23d1240b7e6ad6f4f42396
Katarzyna Ochmanska <kat.ochmanska@gmail.com>
2025-04-05 20:03:16 +0200

    Solution

---
+++ src/main/java/mate/academy/service/impl/OrderServiceImpl.java
@@ -0,0 +1,50 @@
+package mate.academy.service.impl;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import mate.academy.dao.OrderDao;
+import mate.academy.lib.Service;
+import mate.academy.model.Order;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
+import mate.academy.model.User;
+import mate.academy.service.OrderService;
+
+@Service
+@AllArgsConstructor
+public class OrderServiceImpl implements OrderService {
+
+    private final OrderDao orderDao;
+
+    @Override
+    public Order completeOrder(ShoppingCart shoppingCart) {
+        Order newOrder = new Order();
+        newOrder.setUser(shoppingCart.getUser());
+        List<Ticket> tickets = new ArrayList<>(shoppingCart.getTickets());
+        newOrder.setTickets(tickets);
+        LocalDateTime earliestDateOfMovieSession = findEarliestDateOfMovieSession(tickets);
+        newOrder.setOrderDate(earliestDateOfMovieSession.minusDays(3));
+        return orderDao.add(newOrder);
+    }
+
+    @Override
+    public List<Order> getOrdersHistory(User user) {
+        return orderDao.getByUser(user);
+    }
+
+    private LocalDateTime findEarliestDateOfMovieSession(List<Ticket> tickets) {
+        LocalDateTime[] arrShowTimes = new LocalDateTime[tickets.size()];
+        for (int i = 0; i < tickets.size(); i++) {
+            arrShowTimes[i] = tickets.get(i).getMovieSession().getShowTime();
+        }
+        LocalDateTime minDate = arrShowTimes[0];
+        for (LocalDateTime someDate : arrShowTimes) {
+            if (someDate.compareTo(minDate) < 0) {
+                minDate = someDate;
+            }
+        }
+        return minDate;
+    }
+}
